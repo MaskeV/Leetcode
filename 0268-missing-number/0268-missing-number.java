8c6070cf88b1d9a547a96a326f63799a92d220f2
MaskeV <153220050+MaskeV@users.noreply.github.com>
2024-10-25 18:56:35 +0530

    Time: 2 ms (27.83%), Space: 45.7 MB (7.87%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,15 +1,22 @@
 class Solution {
     public int missingNumber(int[] nums) {
-      Arrays.sort(nums);
-      if(nums[0]!=0){
-        return 0;
-      }
-      for(int i=1;i<nums.length;i++){
+    //   Arrays.sort(nums);
+    //   if(nums[0]!=0){
+    //     return 0;
+    //   }
+    //   for(int i=1;i<nums.length;i++){
         
-        if(nums[i]!=i){
-            return i;
-        }
-      }
-        return nums[nums.length-1]+1;
+    //     if(nums[i]!=i){
+    //         return i;
+    //     }
+    //   }
+    //     return nums[nums.length-1]+1;
+
+    int n=nums.length;
+    int sum = (n*(n+1))/2;
+    int actualSum = Arrays.stream(nums).sum();
+    return sum-actualSum;
+
+
     }
 }
