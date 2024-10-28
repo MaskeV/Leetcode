e6e2f67424ba9cbfdf14824fde35256b580847c3
MaskeV <153220050+MaskeV@users.noreply.github.com>
2024-10-28 22:38:36 +0530

    Time: 924 ms (34.74%), Space: 0B (100%) - LeetHub

--- 1251-average-selling-price/1251-average-selling-price.sql
+++ 1251-average-selling-price/1251-average-selling-price.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-select P.product_id ,round(sum(P.price*U.units)/sum(units),2) as average_price
+select P.product_id ,round(ifnull(sum(P.price*U.units)/nullif(sum(units),0),0),2) as average_price
 from Prices P left join UnitsSold U on P.product_id=u.product_id
 and P.start_date<= U.purchase_date and P.end_date >= U.purchase_date
 group by P.product_id;
