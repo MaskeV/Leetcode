db17c178b4b4926c80b95c8e10b0b1135cf6ed80
MaskeV <153220050+MaskeV@users.noreply.github.com>
2024-11-10 22:22:16 +0530

    Time: 284 ms (73.33%), Space: 0B (100%) - LeetHub

---
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+(select salary as SecondHighestSalary
+from Employee
+where salary != (select max(salary) from employee) 
+order by salary desc
+limit 1)
+union
+(SELECT NULL AS SecondHighestSalary
+    FROM Employee
+    HAVING COUNT(distinct salary) <= 1)
