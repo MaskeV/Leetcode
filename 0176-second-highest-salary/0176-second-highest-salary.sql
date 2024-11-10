2dad02948619743b2193071737ad57968af8a2bf
MaskeV <153220050+MaskeV@users.noreply.github.com>
2024-11-10 23:03:18 +0530

    Time: 269 ms (86.26%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,10 +1,15 @@
 # Write your MySQL query statement below
-(select salary as SecondHighestSalary
+-- (select salary as SecondHighestSalary
+-- from Employee
+-- where salary != (select max(salary) from employee) 
+-- order by salary desc
+-- limit 1)
+-- union
+-- (SELECT NULL AS SecondHighestSalary
+-- FROM Employee
+-- HAVING COUNT(distinct salary) <= 1)
+
+
+select max(salary) as SecondHighestSalary
 from Employee
-where salary != (select max(salary) from employee) 
-order by salary desc
-limit 1)
-union
-(SELECT NULL AS SecondHighestSalary
-    FROM Employee
-    HAVING COUNT(distinct salary) <= 1)
+where salary<(select max(salary) from Employee);
