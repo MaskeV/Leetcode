ec492d9f6973d397693c195b77c20717f55658b2
MaskeV <153220050+MaskeV@users.noreply.github.com>
2024-11-28 14:57:01 +0530

    Time: 429 ms (28.12%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -33,8 +33,16 @@
 
 
 
-select  E1.name
-from Employee E1 join Employee E2
-on E1.id = E2.managerId
-group by E1.id
-having count(E1.id)>=5;
+-- select  E1.name
+-- from Employee E1 join Employee E2
+-- on E1.id = E2.managerId
+-- group by E1.id
+-- having count(E1.id)>=5;
+
+select name
+from Employee
+where id in (Select managerId 
+from Employee 
+where managerId is  not null 
+group by managerId 
+having count(managerId)>=5);
