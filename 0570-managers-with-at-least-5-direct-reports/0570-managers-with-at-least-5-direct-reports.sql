b41bd019f85bef5b6bc54ff149a5a9738813de95
MaskeV <153220050+MaskeV@users.noreply.github.com>
2024-11-28 14:53:01 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -3,8 +3,38 @@
 -- where id in (select managerId from Employee where managerId is not NULL group by managerId having count(managerId)>=5);
 
 
-SELECT e.name
-FROM Employee AS e 
-INNER JOIN Employee AS m ON e.id=m.managerId 
-GROUP BY e.id
-HAVING COUNT(e.id) >= 5
+-- SELECT e.name
+-- FROM Employee AS e 
+-- INNER JOIN Employee AS m ON e.id=m.managerId 
+-- GROUP BY e.id
+-- HAVING COUNT(e.id) >= 5
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+select distinct E1.name
+from Employee E1 join Employee E2
+on E1.id = E2.managerId
+group by E1.id
+having count(E1.id)>=5;
