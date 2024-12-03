4036bfbbf1175701970e1ebd615b187e0cc9f309
MaskeV <153220050+MaskeV@users.noreply.github.com>
2024-12-03 21:47:49 +0530

    Time: 0 ms (100%), Space: 42.4 MB (6.57%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,21 +1,43 @@
 class Solution {
     public void sortColors(int[] nums) {
-        Map<Integer,Integer> count = new HashMap<>();
+        // Map<Integer,Integer> count = new HashMap<>();
        
 
 
-        for(int num:nums){
-            count.put(num,count.getOrDefault(num,0)+1);
-        }
+        // for(int num:nums){
+        //     count.put(num,count.getOrDefault(num,0)+1);
+        // }
 
-        int idx =0;
-        for(int i=0;i<3;i++){
-            int freq = count.getOrDefault(i,0);
-            for(int j=0;j<freq;j++){
-                nums[idx] = i;
-                idx++;
+        // int idx =0;
+        // for(int i=0;i<3;i++){
+        //     int freq = count.getOrDefault(i,0);
+        //     for(int j=0;j<freq;j++){
+        //         nums[idx] = i;
+        //         idx++;
                
+        //     }
+        // }
+
+
+        int low=0, high=nums.length-1,curr=0;
+
+        while(curr<=high){
+            if(nums[curr]==0){
+                int temp = nums[low];
+                nums[low]=nums[curr];
+                nums[curr]=temp;
+                low++;
+                curr++;
             }
+          else if(nums[curr]==1){
+            curr++;
+          }else{
+            int temp = nums[high];
+            nums[high]=nums[curr];
+            nums[curr]=temp;
+            high--;
+      
+          }
         }
 
 
