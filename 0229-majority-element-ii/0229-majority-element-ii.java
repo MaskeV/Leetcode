79b46114ace7956207a1dec70ea952f6aaaf9209
MaskeV <153220050+MaskeV@users.noreply.github.com>
2024-12-15 22:40:22 +0530

    Time: 11 ms (42.79%), Space: 47.8 MB (59.11%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,23 +1,38 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        // Map<Integer,Integer> map = new HashMap<>();
-        List<Integer> result = new ArrayList<>();
+        // List<Integer> result = new ArrayList<>();
 
-         HashSet<Integer> set = new HashSet<>();
-         for(int n : nums){
-            set.add(n);
-         }
-        for(int n:set){
-            int count=0;
-            for(int j=0;j<nums.length;j++){
-                if(n==nums[j]){
-                    count++;
-                }
-            }
-            if(count>nums.length/3){
-                result.add(n);
-            }
+        //  HashSet<Integer> set = new HashSet<>();
+        //  for(int n : nums){
+        //     set.add(n);
+        //  }
+        // for(int n:set){
+        //     int count=0;
+        //     for(int j=0;j<nums.length;j++){
+        //         if(n==nums[j]){
+        //             count++;
+        //         }
+        //     }
+        //     if(count>nums.length/3){
+        //         result.add(n);
+        //     }
+        // }
+        // return result;
+
+        HashMap<Integer,Integer> map=new HashMap<>();
+       List<Integer> result = new ArrayList<>();
+
+        for(int i:nums){
+            map.put(i,map.getOrDefault(i,0)+1);
         }
-        return result;
+        for(int k:map.keySet()){
+            if(map.get(k)>nums.length/3){
+                result.add(k);
+            }
+
+        }
+         return result;
+      
+    
     }
 }
