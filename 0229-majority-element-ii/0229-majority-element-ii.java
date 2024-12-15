c60a1d08d507e4e73683eb73082dd0370d2f5cb0
MaskeV <153220050+MaskeV@users.noreply.github.com>
2024-12-15 22:41:05 +0530

    Time: 5 ms (49.18%), Space: 48.1 MB (46.5%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -19,20 +19,65 @@ class Solution {
         // }
         // return result;
 
-        HashMap<Integer,Integer> map=new HashMap<>();
-       List<Integer> result = new ArrayList<>();
+    //     HashMap<Integer,Integer> map=new HashMap<>();
+    //    List<Integer> result = new ArrayList<>();
 
-        for(int i:nums){
-            map.put(i,map.getOrDefault(i,0)+1);
-        }
-        for(int k:map.keySet()){
-            if(map.get(k)>nums.length/3){
-                result.add(k);
-            }
+    //     for(int i:nums){
+    //         map.put(i,map.getOrDefault(i,0)+1);
+    //     }
+    //     for(int k:map.keySet()){
+    //         if(map.get(k)>nums.length/3){
+    //             result.add(k);
+    //         }
 
-        }
-         return result;
+    //     }
+    //      return result;
       
-    
+        Integer majority1 = 0;
+        Integer majority2 = 0;
+        int count1 = 0;
+        int count2 = 0;
+
+        for (int num : nums) {
+            if (num == majority1) {
+                count1++;
+            } else if (num == majority2) {
+                count2++;
+            } else if (count1 == 0) {
+                majority1 = num;
+                count1++;
+            } else if (count2 == 0) {
+                majority2 = num;
+                count2++;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+
+        count1 = 0;
+        count2 = 0;
+
+        for (int num : nums) {
+            if (num == majority1) {
+                count1++;
+            } else if (num == majority2) {
+                count2++;
+            }
+        }
+
+        List<Integer> res = new ArrayList<>();
+        int n = nums.length;
+
+        if (count1 > n / 3) {
+            res.add(majority1);
+        }
+        if (count2 > n / 3) {
+            res.add(majority2);
+        }
+
+        return res;        
     }
 }
+    
+   
