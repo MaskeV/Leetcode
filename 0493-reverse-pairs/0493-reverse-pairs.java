47f1207564e3e1abf63f1bad6273cedfb9d7e46a
MaskeV <153220050+MaskeV@users.noreply.github.com>
2024-12-18 21:56:13 +0530

    Time: 166 ms (5.14%), Space: 55.2 MB (29.64%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.java
+++ 0493-reverse-pairs/0493-reverse-pairs.java
@@ -13,19 +13,59 @@
 //     }
 // }
 
-public class Solution {
-    public int reversePairs(int[] nums) {
-        return mergeSort(nums, 0, nums.length-1);
-    }
-    private int mergeSort(int[] nums, int s, int e){
-        if(s>=e) return 0; 
-        int mid = s + (e-s)/2; 
-        int cnt = mergeSort(nums, s, mid) + mergeSort(nums, mid+1, e); 
-        for(int i = s, j = mid+1; i<=mid; i++){
-            while(j<=e && nums[i]/2.0 > nums[j]) j++; 
-            cnt += j-(mid+1); 
+class Solution {
+    private int merge(int l, int mid, int r, List<Integer> nums) {
+        List<Integer> temp = new ArrayList<>();
+        int i = l, j = mid + 1, count = 0;
+
+        // Counting reverse pairs
+        while (i <= mid && j <= r) {
+            if ((long) nums.get(i) > 2L * nums.get(j)) {
+                count += mid - i + 1;
+                j++;
+            } else {
+                i++;
+            }
         }
-        Arrays.sort(nums, s, e+1); 
-        return cnt; 
+
+        i = l;
+        j = mid + 1;
+
+        // Merging the sorted subarrays
+        while (i <= mid && j <= r) {
+            if (nums.get(i) <= nums.get(j)) {
+                temp.add(nums.get(i++));
+            } else {
+                temp.add(nums.get(j++));
+            }
+        }
+        
+        while (i <= mid) temp.add(nums.get(i++));
+        while (j <= r) temp.add(nums.get(j++));
+
+        for (int k = l; k <= r; k++) {
+            nums.set(k, temp.get(k - l));
+        }
+
+        return count;
+    }
+
+    private int mergeSort(int l, int r, List<Integer> nums) {
+        if (l >= r) return 0;
+
+        int count = 0;
+        int mid = l + (r - l) / 2;
+
+        count += mergeSort(l, mid, nums);
+        count += mergeSort(mid + 1, r, nums);
+        count += merge(l, mid, r, nums);
+
+        return count;
+    }
+
+    public int reversePairs(int[] nums) {
+        List<Integer> numList = new ArrayList<>();
+        for (int num : nums) numList.add(num);
+        return mergeSort(0, nums.length - 1, numList);
     }
 }
