a9faa2da88285bf22ff60b18bcfeb8a5dbf1910e
MaskeV <153220050+MaskeV@users.noreply.github.com>
2025-02-07 22:45:37 +0530

    Time: 205 ms (68.15%), Space: 80.2 MB (12.1%) - LeetHub

--- 1726-tuple-with-same-product/1726-tuple-with-same-product.java
+++ 1726-tuple-with-same-product/1726-tuple-with-same-product.java
@@ -1,36 +1,45 @@
+
+
+import java.util.*;
+
 class Solution {
-
     public int tupleSameProduct(int[] nums) {
-        int numsLength = nums.length;
-        Arrays.sort(nums);
+        // Map<Integer, Integer> productCount = new HashMap<>();
+        // int count = 0;
 
-        int totalNumberOfTuples = 0;
+        // // Iterate through all pairs (i, j) and store their product in the map
+        // for (int i = 0; i < nums.length; i++) {
+        //     for (int j = i + 1; j < nums.length; j++) {
+        //         int product = nums[i] * nums[j];
+                
+        //         // If the product already exists, add its count to result
+        //         count += productCount.getOrDefault(product, 0);
+                
+        //         // Increase the product count
+        //         productCount.put(product, productCount.getOrDefault(product, 0) + 1);
+        //     }
+        // }
+        
+        // return count * 8;  // Each tuple can be arranged in 8 ways
 
-       
-        for (int aIndex = 0; aIndex < numsLength; aIndex++) {
-          
-            for (int bIndex = numsLength - 1; bIndex >= aIndex + 1; bIndex--) {
-                int product = nums[aIndex] * nums[bIndex];
 
-                Set<Integer> possibleDValues = new HashSet<>();
+        Map <Integer,Integer> productcount = new HashMap<>();
 
-               
-                for (int cIndex = aIndex + 1; cIndex < bIndex; cIndex++) {
-                 
-                    if (product % nums[cIndex] == 0) {
-                        int dValue = product / nums[cIndex];
+        int count =0;
 
-                     
-                        if (possibleDValues.contains(dValue)) {
-                            totalNumberOfTuples += 8;
-                        }
+        for(int i=0;i<nums.length-1;i++){
+            for(int j=i+1;j<nums.length;j++){
+                int product = nums[i]*nums[j];
 
-                        possibleDValues.add(nums[cIndex]);
-                    }
-                }
+                count= count + productcount.getOrDefault(product,0);
+
+                productcount.put(product,productcount.getOrDefault(product,0)+1);
+
+
+
+
             }
         }
-
-        return totalNumberOfTuples;
+        return count*8;
     }
 }
