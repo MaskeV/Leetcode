b818cdb569becb8b6189aca679bc0db25cdbb7c6
MaskeV <153220050+MaskeV@users.noreply.github.com>
2025-02-10 06:44:40 +0530

    Time: 1 ms (83.66%), Space: 41.7 MB (71.96%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -1,21 +1,61 @@
-class Solution {
-    public String mergeAlternately(String word1, String word2) {
-        StringBuilder s = new StringBuilder();
-        int i=0,j=0,k=0;
-        while(i<word1.length() && j<word2.length()){
-            s.append(word1.charAt(i));
-            s.append(word2.charAt(j));
-            i++;
-            j++;     
-        }
-        while(i<word1.length()){
-            s.append(word1.charAt(i));
-            i++;
-        }
-        while(j<word2.length()){
-            s.append(word2.charAt(j));
-            j++;
-        }
-        return s.toString();
+// class Solution {
+//     public String mergeAlternately(String word1, String word2) {
+//         StringBuilder s = new StringBuilder();
+//         int i=0,j=0,k=0;
+//         while(i<word1.length() && j<word2.length()){
+//             s.append(word1.charAt(i));
+//             s.append(word2.charAt(j));
+//             i++;
+//             j++;     
+//         }
+//         while(i<word1.length()){
+//             s.append(word1.charAt(i));
+//             i++;
+//         }
+//         while(j<word2.length()){
+//             s.append(word2.charAt(j));
+//             j++;
+//         }
+//         return s.toString();
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+ class Solution {
+   public String mergeAlternately(String word1, String word2) {
+   int i=0,j=0,k=0;
+   StringBuilder s = new StringBuilder();
+    int n1=word1.length(),n2=word2.length();
+
+
+  while(i<n1 && j<n2){
+    s.append(word1.charAt(i));
+    s.append(word2.charAt(j));
+    i++;
+    j++;
     }
-}
+
+   while(i<n1){
+    s.append(word1.charAt(i));
+    i++;
+
+   }
+
+   while(j<n2){
+    s.append(word2.charAt(j));
+    j++;
+   }
+ return s.toString();
+
+   }
+ }
