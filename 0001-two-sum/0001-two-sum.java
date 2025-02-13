fdf802e17c53b35a35073b72fd115e74a5b86660
MaskeV <153220050+MaskeV@users.noreply.github.com>
2025-02-13 22:23:12 +0530

    Time: 2 ms (98.91%), Space: 45.1 MB (29.17%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -6,9 +6,18 @@ class Solution {
         if(map.containsKey(comp)){
             return new int[] {map.get(comp),i};
         }
+        
         map.put(nums[i],i);
      }
     return new int[0] ;
+    // for(int i=0;i<nums.length-1;i++){
+    //     for(int j=i+1;j<nums.length;j++){
+    //             if(nums[j]==target - nums[i]){
+    //                 return new int[] {i,j};
+    //             }
+    //     }
+    // }
+    // return new int[0];
     }
 }
 
