c351beef84fa0876caf1121228d2c1b7eb05c255
MaskeV <153220050+MaskeV@users.noreply.github.com>
2025-02-15 18:57:46 +0530

    Time: 1043 ms (7.05%), Space: 68.6 MB (88.46%) - LeetHub

--- 1352-product-of-the-last-k-numbers/1352-product-of-the-last-k-numbers.java
+++ 1352-product-of-the-last-k-numbers/1352-product-of-the-last-k-numbers.java
@@ -1,27 +1,27 @@
-//  class ProductOfNumbers {
-//      List<Integer> list;
-//     public ProductOfNumbers() {
-//          list = new ArrayList<>();
+ class ProductOfNumbers {
+     ArrayList<Integer> list;
+    public ProductOfNumbers() {
+         list = new ArrayList<>();
             
-//     }
+    }
     
-//     public void add(int num) {
-//         list.add(num);
+    public void add(int num) {
+        list.add(num);
         
-//     }
+    }
     
-//     public int getProduct(int k) {
-//         int ans=1;
+    public int getProduct(int k) {
+        int ans=1;
       
 
-//         for(int i=list.size()-k;i<list.size();i++){
-//           ans*=list.get(i);
+        for(int i=list.size()-k;i<list.size();i++){
+          ans*=list.get(i);
 
-//         }
+        }
 
-//         return  ans;
-//     }
-// }
+        return  ans;
+    }
+}
 
 /**
  * Your ProductOfNumbers object will be instantiated and called as such:
@@ -30,22 +30,22 @@
  * int param_2 = obj.getProduct(k);
  */
 
- class ProductOfNumbers {
+//  class ProductOfNumbers {
 
-    ArrayList<Integer> al;
-    public ProductOfNumbers() {
-       this.al = new ArrayList<>();
-    }
+//     ArrayList<Integer> al;
+//     public ProductOfNumbers() {
+//        this.al = new ArrayList<>();
+//     }
     
-    public void add(int num) {
-        al.add(num);
-    }
+//     public void add(int num) {
+//         al.add(num);
+//     }
     
-    public int getProduct(int k) {
-        int p=1;
-        for(int i=al.size()-k; i<al.size(); i++){
-            p*=al.get(i);
-        }
-        return p;
-    }
-}
+//     public int getProduct(int k) {
+//         int p=1;
+//         for(int i=al.size()-k; i<al.size(); i++){
+//             p*=al.get(i);
+//         }
+//         return p;
+//     }
+// }
