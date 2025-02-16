90043daace48a03bb6f5c6837f8f759e698c6f9b
MaskeV <153220050+MaskeV@users.noreply.github.com>
2025-02-16 22:37:48 +0530

    Time: 1 ms (100%), Space: 42.2 MB (100%) - LeetHub

--- 3456-find-special-substring-of-length-k/3456-find-special-substring-of-length-k.java
+++ 3456-find-special-substring-of-length-k/3456-find-special-substring-of-length-k.java
@@ -7,43 +7,46 @@
 //                         return true;
 //                     }
 //                 }else{
+//                     if(j-i==k){
+//                         return true;
+//                         }
 //                 i=j;  
 //                 }           
 
 //         }
-//         return false;
-//     }
-// }
-
-// class Solution {
-//     public boolean hasSpecialSubstring(String s, int k) {
-//         int i =0;
-//         for(int j=0;j<s.length();j++){
-//             if(s.charAt(j)==s.charAt(i)) continue;
-//             if(j-i==k) return true;
-//             i=j;
-//         }
 //         return (s.length()-i)==k;
-
 //     }
 // }
 
 class Solution {
     public boolean hasSpecialSubstring(String s, int k) {
-        int i = 0; // Start of the current repeating substring
-        
-        for (int j = 0; j < s.length(); j++) {
-            if (s.charAt(j) != s.charAt(i)) {
-                // Before resetting i, check if the previous substring had length k
-                if (j - i == k) {
-                    return true;
-                }
-                i = j; // Update start of new substring
-            }
+        int i =0;
+        for(int j=0;j<s.length();j++){
+            if(s.charAt(j)==s.charAt(i)) continue;
+            if(j-i==k) return true;
+            i=j;
         }
-        
-        // Final check if last substring is of length k
-        return (s.length() - i) == k;
+        return (s.length()-i)==k;
+
     }
 }
+
+// class Solution {
+//     public boolean hasSpecialSubstring(String s, int k) {
+//         int i = 0; // Start of the current repeating substring
+        
+//         for (int j = 0; j < s.length(); j++) {
+//             if (s.charAt(j) != s.charAt(i)) {
+//                 // Before resetting i, check if the previous substring had length k
+//                 if (j - i == k) {
+//                     return true;
+//                 }
+//                 i = j; // Update start of new substring
+//             }
+//         }
+        
+//         // Final check if last substring is of length k
+//         return (s.length() - i) == k;
+//     }
+// }
 
