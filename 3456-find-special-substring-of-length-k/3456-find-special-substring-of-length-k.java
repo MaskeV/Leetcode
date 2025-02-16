2079e9aae0a014b003b8c7afc02dcbeaa09fb35f
MaskeV <153220050+MaskeV@users.noreply.github.com>
2025-02-16 22:15:37 +0530

    Time: 1 ms (100%), Space: 42.1 MB (100%) - LeetHub

--- 3456-find-special-substring-of-length-k/3456-find-special-substring-of-length-k.java
+++ 3456-find-special-substring-of-length-k/3456-find-special-substring-of-length-k.java
@@ -1,37 +1,49 @@
 // class Solution {
 //     public boolean hasSpecialSubstring(String s, int k) {
 //         int i=0;
-//         if( k==1){
-//             return true;
-//         }
-//         while(i<=s.length()-k){
-//             int j=1;
-//             while(j!=k ) {
-//                 if(s.charAt(i)==(s.charAt(i+j))){
-//                     if(i+j==i+k-1){
+//                 for(int j=0;j<s.length();j++){
+//                 if(s.charAt(j)==(s.charAt(i))){
+//                     if(j-i==k){
 //                         return true;
 //                     }
-//                     j++;
-                    
 //                 }else{
-//                     break;
-//                 }
-//             }
-//           i= i+k;
+//                 i=j;  
+//                 }           
+
 //         }
 //         return false;
 //     }
 // }
 
+// class Solution {
+//     public boolean hasSpecialSubstring(String s, int k) {
+//         int i =0;
+//         for(int j=0;j<s.length();j++){
+//             if(s.charAt(j)==s.charAt(i)) continue;
+//             if(j-i==k) return true;
+//             i=j;
+//         }
+//         return (s.length()-i)==k;
+
+//     }
+// }
+
 class Solution {
     public boolean hasSpecialSubstring(String s, int k) {
-        int i =0;
-        for(int j=0;j<s.length();j++){
-            if(s.charAt(j)==s.charAt(i)) continue;
-            if(j-i==k) return true;
-            i=j;
+        int i = 0; // Start of the current repeating substring
+        
+        for (int j = 0; j < s.length(); j++) {
+            if (s.charAt(j) != s.charAt(i)) {
+                // Before resetting i, check if the previous substring had length k
+                if (j - i == k) {
+                    return true;
+                }
+                i = j; // Update start of new substring
+            }
         }
-        return (s.length()-i)==k;
-
+        
+        // Final check if last substring is of length k
+        return (s.length() - i) == k;
     }
 }
+
