41d3ed74cbeaa2a0f4d968cbe1a2ec322d006067
MaskeV <153220050+MaskeV@users.noreply.github.com>
2025-04-17 23:17:58 +0530

    Time: 1 ms (82.4%), Space: 41.8 MB (57.8%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -16,18 +16,31 @@ class Solution {
 //             s.append(word2.charAt(j));
 //             j++;
 //         }
-int m=word1.length(),n=word2.length();
+// int m=word1.length(),n=word2.length();
 
 
-for(int i=0;i<Math.max(m,n);i++){
-    if(i<m){
-        s.append(word1.charAt(i));
-    }if(i<n){
-        s.append(word2.charAt(i));
+// for(int i=0;i<Math.max(m,n);i++){
+//     if(i<m){
+//         s.append(word1.charAt(i));
+//     }if(i<n){
+//         s.append(word2.charAt(i));
+//     }
+
+// }
+//         return s.toString();
+int i=0,j=0,k=0;
+
+while(i<word1.length() || j<word2.length()){
+    if(i<word1.length()){
+    s.append(word1.charAt(i));
     }
-
+    if(j<word2.length()){
+    s.append(word2.charAt(j));
+    }
+    i++;
+    j++;
 }
-        return s.toString();
+return s.toString();
     }
 }
 
