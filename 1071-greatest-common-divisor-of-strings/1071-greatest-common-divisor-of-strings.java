196c211dfc48e620bc001e51eda91c2db99b103d
MaskeV <153220050+MaskeV@users.noreply.github.com>
2025-04-17 23:50:29 +0530

    Time: 1 ms (71.47%), Space: 42.3 MB (46.43%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
@@ -1,25 +1,48 @@
 class Solution {
     public String gcdOfStrings(String str1, String str2) {
-        StringBuilder s = new StringBuilder();
-        if(!((str1+str2).equals(str2+str1))){
-            return "";
-        }
-      int gcdLength=gcd(str1.length(),str2.length());
-        for(int i=0;i<gcdLength;i++){
-            s.append(str2.charAt(i));
-        }
-        return s.toString();
-    }
+    //     StringBuilder s = new StringBuilder();
+    //     if(!((str1+str2).equals(str2+str1))){
+    //         return "";
+    //     }
+    //   int gcdLength=gcd(str1.length(),str2.length());
+    //     for(int i=0;i<gcdLength;i++){
+    //         s.append(str2.charAt(i));
+    //     }
+    //     return s.toString();
+    // }
 
-    private int gcd(int n,int m){
+    // private int gcd(int n,int m){
        
-       int result=Math.min(n,m);
-       while(result>0){
-       if(n%result==0 && m%result==0){
-        return result;
-       }
-       result--;
-       }
-       return 1;
+    //    int result=Math.min(n,m);
+    //    while(result>0){
+    //    if(n%result==0 && m%result==0){
+    //     return result;
+    //    }
+    //    result--;
+    //    }
+    //    return 1;
+
+    StringBuilder s = new StringBuilder();
+    if(!((str1+str2).equals(str2+str1))){
+        return "";
     }
+    int gcdlength = gcd(str1.length(),str2.length());
+    for(int i=0;i<gcdlength;i++){
+        s.append(str2.charAt(i));
+    }
+return s.toString();
+
+    }
+    private int gcd(int m,int n){
+        int result = Math.min(m,n);
+        while(result>0){
+            if(n%result==0 && m%result==0){
+                return result;
+            }
+            result--;
+        }
+       return 1;
+
+    }
+   
 }
