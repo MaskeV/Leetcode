a561a30f04722c1552908a4caf4f2d92076b2126
Vaishnavi Maske <vmaske7071@gmail.com>
2025-04-22 00:01:43 +0530

    Time: 1 ms (97.6%), Space: 46.2 MB (7.28%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.java
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -1,33 +1,52 @@
 class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
-        int len = flowerbed.length;
-        int canPlant=0;
+    //     int len = flowerbed.length;
+    //     int canPlant=0;
 
-        if(len==1 && flowerbed[0]==0 && n==1){
-            return true;
-        }
+    //     if(len==1 && flowerbed[0]==0 && n==1){
+    //         return true;
+    //     }
 
-        if(len>1 && flowerbed[0]==0){
-            if(flowerbed[1]==0){
-                flowerbed[0]=1;
-                canPlant++;
+    //     if(len>1 && flowerbed[0]==0){
+    //         if(flowerbed[1]==0){
+    //             flowerbed[0]=1;
+    //             canPlant++;
+    //         }
+    //     }
+
+    //     for(int i=1;i<len-1;i++){
+    //         if(flowerbed[i-1]==0 && flowerbed[i]==0 && flowerbed[i+1]==0){
+    //             flowerbed[i]=1;
+    //             canPlant++;
+    //         } 
+    //     }
+        
+    //     if(len>1 && flowerbed[len-1]==0){
+    //       if(flowerbed[len-2]==0){
+    //         flowerbed[len-2]=1;
+    //         canPlant++;
+    //       }
+
+    //     }
+
+    //     return canPlant >= n;
+    // }
+
+    if(n==0){
+        return true;
+    }
+    int l = flowerbed.length;
+
+    for(int i=0;i<l;i++){
+        if(flowerbed[i]==0 && (i==0||flowerbed[i-1]==0) && (i==l-1 ||flowerbed[i+1]==0)){
+            n--;
+            if(n==0){
+                return true;
             }
-        }
 
-        for(int i=1;i<len-1;i++){
-            if(flowerbed[i-1]==0 && flowerbed[i]==0 && flowerbed[i+1]==0){
-                flowerbed[i]=1;
-                canPlant++;
-            } 
+        flowerbed[i]=1;
         }
-        if(len>1 && flowerbed[len-1]==0){
-          if(flowerbed[len-2]==0){
-            flowerbed[len-2]=1;
-            canPlant++;
-          }
-
-        }
-
-        return canPlant >= n;
+    }
+    return false;
     }
 }
