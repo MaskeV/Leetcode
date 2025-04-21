423834638700f48cb6478bbf3e6da08ed377c456
Vaishnavi Maske <vmaske7071@gmail.com>
2025-04-22 00:07:53 +0530

    Time: 1 ms (97.6%), Space: 45.5 MB (76.67%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.java
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -38,7 +38,7 @@ class Solution {
     int l = flowerbed.length;
 
     for(int i=0;i<l;i++){
-        if(flowerbed[i]==0 && (i==0||flowerbed[i-1]==0) && (i==l-1 ||flowerbed[i+1]==0)){
+        if(flowerbed[i]==0 && (i==0 || flowerbed[i-1]==0) && (i==l-1 || flowerbed[i+1]==0)){
             n--;
             if(n==0){
                 return true;
