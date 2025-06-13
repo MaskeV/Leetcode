77b0adc7a3453e521b470532494acc859ae43665
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2025-06-13 20:25:14 +0530

    Time: 2 ms (98.86%), Space: 44.7 MB (88.74%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -6,10 +6,10 @@ class Solution {
         if(map.containsKey(target-nums[i])){
             return new int[] {map.get(target-nums[i]),i};
         }
-        map.put(nums[i],i);
+     map.put(nums[i],i);
      }
-    return new int[0] ;
-    // for(int i=0;i<nums.length-1;i++){
+     return new int[0];
+         // for(int i=0;i<nums.length-1;i++){
     //     for(int j=i+1;j<nums.length;j++){
     //             if(nums[j]==target - nums[i]){
     //                 return new int[] {i,j};
