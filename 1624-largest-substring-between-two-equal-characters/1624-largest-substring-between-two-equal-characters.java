506203d4e8daa1ebec18edca9d27286d730832d5
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2025-06-15 23:33:57 +0530

    Time: 2 ms (68.97%), Space: 41.7 MB (28.6%) - LeetHub

--- 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.java
+++ 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.java
@@ -22,8 +22,9 @@ class Solution {
    for(int i=0;i<s.length();i++){
     if(map.containsKey(s.charAt(i))){
         ans=Math.max(ans,i-map.get(s.charAt(i))-1);
-    }
+    }else{
     map.put(s.charAt(i),i);
+    }
 
    }
 
