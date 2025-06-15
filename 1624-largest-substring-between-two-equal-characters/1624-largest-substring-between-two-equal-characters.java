ef0077a49206699bbf4c67eabf21974bf9fa7b72
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2025-06-15 22:58:58 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.java
+++ 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.java
@@ -1,29 +1,32 @@
 class Solution {
     public int maxLengthBetweenEqualCharacters(String s) {
-        
-    //     class Solution {
-    // public int maxLengthBetweenEqualCharacters(String s) {
-    //     int ans = -1;
-    //     for (int left = 0; left < s.length(); left++) {
-    //         for (int right = left + 1; right < s.length(); right++) {
-    //             if (s.charAt(left) == s.charAt(right)) {
-    //                 ans = Math.max(ans, right - left - 1);
-    //             }
-    //         }
-    //     }
-        
-    //     return ans;
+   
     int ans=-1;
 
-    for(int left=0;left<s.length();left++ ){
-        for(int right=left+1;right<s.length();right++){
-            if(s.charAt(left)==s.charAt(right)){
-                ans=Math.max(ans,right-left-1);
-            }
+    //brute force(o(n^2))
 
-        }
+    // for(int left=0;left<s.length();left++ ){
+    //     for(int right=left+1;right<s.length();right++){
+    //         if(s.charAt(left)==s.charAt(right)){
+    //             ans=Math.max(ans,right-left-1);
+    //         }
+
+    //     }
     
+    // }
+   
+
+   //Linear (o(n))
+
+   Map<Character,Integer> map = new HashMap<>();
+   for(int i=0;i<s.length();i++){
+    if(map.containsKey(s.charAt(i))){
+        ans=Math.max(ans,i-map.get(s.charAt(i))-1);
     }
+    map.put(s.charAt(i),i);
+
+   }
+
     return ans;
 
 
