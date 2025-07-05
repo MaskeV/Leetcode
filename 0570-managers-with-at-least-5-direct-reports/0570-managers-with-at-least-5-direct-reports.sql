bfeb3362c34c9e5ed28745777980843f50680035
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2025-07-05 11:08:26 +0530

    Time: 340 ms (65.74%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -11,15 +11,18 @@
 
 
 
-select distinct E.name
+-- select E.name
+-- from Employee E left join Employee M
+-- on E.id=M.managerId
+-- group by E.id
+-- having count(E.id)>=5
+
+
+select  M.name
 from Employee E left join Employee M
-on E.id=M.managerId
-group by E.id
-having count(E.id)>=5
-
-
-
-
+on E.managerId=M.id
+group by M.id
+having count(M.id)>=5
 
 
 
