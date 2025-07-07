43957d2c7d9345f565b6221e80ad548c4ec8cc8e
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2025-07-07 13:57:03 +0530

    Time: 522 ms (76.48%), Space: 0B (100%) - LeetHub

--- 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -12,11 +12,17 @@
 
 
 
-select employee_id, department_id
-from Employee
-where primary_flag ='Y' or 
-employee_id in 
- (select employee_id 
- from Employee 
- group by employee_id 
- having count(employee_id)=1);
+-- select employee_id, department_id
+-- from Employee
+-- where 
+-- (employee_id, department_id) in 
+--  (select employee_id , department_id
+--  from Employee 
+--  group by employee_id 
+--  having primary_flag ='Y' or count(employee_id)=1 );
+
+
+
+select employee_id,if(count(department_id)=1,department_id,(select department_id from Employee where employee_id=E.employee_id and primary_flag='Y')) as department_id
+from Employee E
+group by employee_id;
