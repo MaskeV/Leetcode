6b863b8ad6f1bb10608af907d3ce110103fe662f
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2025-07-07 12:41:41 +0530

    Time: 525 ms (7.21%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -11,11 +11,11 @@
 
 
 
--- select E.name
--- from Employee E left join Employee M
--- on E.id=M.managerId
--- group by E.id
--- having count(E.id)>=5
+select E.name
+from Employee E left join Employee M
+on E.id=M.managerId
+group by E.id
+having count(E.id)>=5
 
 
 -- select  M.name
@@ -30,11 +30,19 @@
 -- where id in (select managerId from Employee  group by managerId having count(managerId)>=5);
 
 
-SELECT m.name
-FROM Employee e  JOIN Employee m 
-ON e.managerId=m.id
-GROUP BY e.managerId
-HAVING COUNT(e.managerId) >= 5
+-- SELECT m.name
+-- FROM Employee e  JOIN Employee m 
+-- ON e.managerId=m.id
+-- GROUP BY e.managerId
+-- HAVING COUNT(e.managerId) >= 5
+
+
+-- select M.name
+-- from Employee E join Employee M
+-- on E.managerId=M.id
+-- group by M.id
+-- having count(M.id)>=5
+
 
 
 
