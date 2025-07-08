95e15fda2edf002084bbaa11bb4c23833d3f3a83
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2025-07-08 22:27:27 +0530

    Time: 2 ms (80.42%), Space: 46.3 MB (23.31%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,15 +1,31 @@
+// class Solution {
+//    public void moveZeroes(int[] nums) {
+//     int [] ans = new int [nums.length];
+//     int j=0;
+//     for(int num:nums){
+//         if(num!=0){
+//             ans[j]=num;
+//             j++;
+//         }
+//     }
+//     for(int i=0;i<nums.length;i++){
+//        nums[i]=ans[i];
+//     }
+    
+//      }
+// }
+
 class Solution {
-   public void moveZeroes(int[] nums) {
-        int snowBallSize = 0; 
-        for (int i=0;i<nums.length;i++){
-	        if (nums[i]==0){
-                snowBallSize++; 
+    public void moveZeroes(int[] nums) {
+        int left = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] != 0) {
+                int temp = nums[right];
+                nums[right] = nums[left];
+                nums[left] = temp;
+                left++;
             }
-            else if (snowBallSize > 0) {
-	            int t = nums[i];
-	            nums[i]=0;
-	            nums[i-snowBallSize]=t;
-            }
-        }
-    }   
+        }        
     }
+}
