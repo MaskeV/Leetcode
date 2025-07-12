6d58e455c43a47b7b32ed4afa1e44316ca843bf0
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2025-07-12 23:47:09 +0530

    Time: 834 ms (16.59%), Space: 0B (100%) - LeetHub

--- 1517-find-users-with-valid-e-mails/1517-find-users-with-valid-e-mails.sql
+++ 1517-find-users-with-valid-e-mails/1517-find-users-with-valid-e-mails.sql
@@ -1,3 +1,4 @@
-SELECT *
+SELECT user_id, name, mail
 FROM Users
-WHERE mail REGEXP '^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode[.]com$';
+WHERE mail REGEXP '^[A-Za-z][A-Za-z0-9_.-]*@leetcode\\.com$'
+  AND mail LIKE BINARY '%@leetcode.com';
