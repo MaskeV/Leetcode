d405c34a82de1d4beefbf8e3487a942fbcde4611
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2025-07-13 19:11:45 +0530

    Time: 1180 ms (29.03%), Space: 0B (100%) - LeetHub

--- 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -1,9 +1,25 @@
-# Write your MySQL query statement below
-select D.name as Department , E.name as Employee ,Salary
-from Employee E left join Department D on E.departmentId = D.id
-where (
-    select count(distinct salary)
-    from Employee E1
-    where E1.departmentId = E.departmentId AND e1.salary >= e.salary
+-- # Write your MySQL query statement below
+-- select D.name as Department , E.name as Employee ,Salary
+-- from Employee E left join Department D on E.departmentId = D.id
+-- where (
+--     select count(distinct salary)
+--     from Employee E1
+--     where E1.departmentId = E.departmentId AND e1.salary >= e.salary
+-- )<=3
+-- order by departmentId, salary  desc
+
+
+
+
+
+select D.name as Department , E.name as Employee,salary
+from Employee E left join Department D
+on E.departmentId = D.id
+where
+(select count(distinct salary )
+from Employee e1
+where e1.departmentId=E.departmentId and e1.salary >= E.salary
 )<=3
-order by departmentId, salary  desc
+order by departmentId,salary desc 
+
+
