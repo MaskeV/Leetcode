90491b20951b0741a890368c55e77ceca6cc43b8
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2025-07-13 19:51:52 +0530

    Time: 386 ms (24.76%), Space: 0B (100%) - LeetHub

--- 1321-restaurant-growth/1321-restaurant-growth.sql
+++ 1321-restaurant-growth/1321-restaurant-growth.sql
@@ -10,3 +10,16 @@ where visited_on>= (
 select date_add(min(visited_on),Interval 6 day) 
 from Customer)
 group by visited_on
+
+-- select visited_on,(select sum(amount) from Customer 
+-- where visited_on 
+-- between date_sub(C.visited_on,Interval 6 day) and C.visited_on) as amount, 
+-- (select round(avg(amount),2) from Customer
+-- where visited_on
+-- between date_sub(C.visited_on, Interval 6  day) and C.visited_on) as average_amount
+-- from Customer C
+-- where visited_on>= (
+-- select date_add(min(visited_on),Interval 6 day) 
+-- from Customer)
+-- group by visited_on
+
