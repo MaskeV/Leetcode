7d2d9426677770d2e7d3a82e0cfcc6fcae57331d
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2025-07-13 18:47:01 +0530

    Time: 319 ms (39.5%), Space: 0B (100%) - LeetHub

--- 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -1,21 +1,19 @@
 # Write your MySQL query statement below
+-- select requester_id as id, count(requester_id) as num
+-- from RequestAccepted
+-- where requester_id in 
+-- (select requester_id from RequestAccepted
+-- union all
+-- select accepter_id  from RequestAccepted)
+-- group by requester_id
+-- order by count(requester_id) desc
+-- limit 1;
 
 
 select requester_id as id, count(requester_id) as num
-from
-(select requester_id from RequestAccepted 
+from (select requester_id from RequestAccepted 
 Union all
-Select accepter_id  from RequestAccepted) as T
+Select accepter_id  from RequestAccepted) Temp
 group by requester_id
 order by count(requester_id ) desc
 limit 1;
-
--- select requester_id as id, count(requester_id) as num
--- from RequestAccepted
--- where (requester_id) in 
--- (select requester_id from RequestAccepted 
--- Union all
--- Select accepter_id  from RequestAccepted)
--- group by requester_id
--- order by count(requester_id ) desc
--- limit 1;
