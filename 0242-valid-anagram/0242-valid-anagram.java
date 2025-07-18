88cc9a32797a897a86d4c71406ae4f31c3203238
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2025-07-18 23:00:01 +0530

    Time: 3 ms (91.38%), Space: 44.7 MB (34.23%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,26 +1,21 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        char []arr1 = s.toCharArray();
+        char [] arr1 = s.toCharArray();
         Arrays.sort(arr1);
-        char []arr2 = t.toCharArray();
+        char [] arr2 = t.toCharArray();
         Arrays.sort(arr2);
-        int count=0;
-        if(s.length()!=t.length()){
+        if(arr1.length!=arr2.length){
             return false;
         }
-        for(int i=0;i<s.length();i++){
-            if(arr1[i]==arr2[i]){
-                continue;
-            }
-            else{
-                count++;
-            }
+    
+        for(int i=0;i<arr1.length;i++){
+            
+                if(arr1[i]!=arr2[i]){
+                    return false;
+                }
+            
         }
-        if(count==0){
-            return true;
-        }else{
-            return false;
-        }
+        return true;
     }
 }
         
