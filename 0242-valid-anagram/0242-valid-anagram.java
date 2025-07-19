0f211dbd60cf9f57e442b486b1176347a909a144
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2025-07-19 19:31:33 +0530

    Time: 4 ms (77.29%), Space: 42.9 MB (88.38%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,19 +1,33 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        char [] arr1 = s.toCharArray();
-        Arrays.sort(arr1);
-        char [] arr2 = t.toCharArray();
-        Arrays.sort(arr2);
-        if(arr1.length!=arr2.length){
-            return false;
-        }
+        // char [] arr1 = s.toCharArray();
+        // Arrays.sort(arr1);
+        // char [] arr2 = t.toCharArray();
+        // Arrays.sort(arr2);
+        // if(arr1.length!=arr2.length){
+        //     return false;
+        // }
     
-        for(int i=0;i<arr1.length;i++){
+        // for(int i=0;i<arr1.length;i++){
             
-                if(arr1[i]!=arr2[i]){
-                    return false;
-                }
+        //         if(arr1[i]!=arr2[i]){
+        //             return false;
+        //         }
             
+        // }
+        // return true;
+
+        int [] alphabate = new int[26];
+        for(int i=0;i<s.length();i++){
+            alphabate[s.charAt(i)-'a']++; 
+        }
+        for(int i=0;i<t.length();i++){
+            alphabate[t.charAt(i)-'a']--;
+        }
+        for(int i:alphabate){
+            if(i!=0){
+                return false;
+            }
         }
         return true;
     }
