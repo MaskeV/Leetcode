910236833bf146627335a4590ec97d76200b951a
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2025-07-23 22:49:08 +0530

    Time: 2 ms (98.04%), Space: 41.9 MB (70.28%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
@@ -1,18 +1,31 @@
+// class Solution {
+//     public boolean uniqueOccurrences(int[] arr) {
+//       Map<Integer,Integer> map = new  HashMap<>();
+//       Set<Integer> set = new HashSet<>();
+
+//       for(int i:arr){
+//         map.put(i,map.getOrDefault(i,0)+1);
+//       }
+//       for(int i : map.keySet()){
+//         if(!set.add(map.get(i))){
+//             return false;
+//         }
+
+//       }
+//       return true;
+
+//     }
+// }
+
 class Solution {
     public boolean uniqueOccurrences(int[] arr) {
-      Map<Integer,Integer> map = new  HashMap<>();
+       Map<Integer,Integer> map = new  HashMap<>();
       Set<Integer> set = new HashSet<>();
 
       for(int i:arr){
-        map.put(i,map.getOrDefault(i,0)-1);
+        map.put(i,map.getOrDefault(i,0)+1);
       }
-      for(int i : map.keySet()){
-        if(!set.add(map.get(i))){
-            return false;
-        }
-
-      }
-      return true;
-
+        return map.size() == new HashSet<>(map.values()).size();
+      
     }
 }
