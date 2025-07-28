cd26412e7530c25910c4db54fd508b684e798f1b
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2025-07-28 19:58:54 +0530

    Time: 462 ms (42.47%), Space: 0B (100%) - LeetHub

--- 1484-group-sold-products-by-the-date/1484-group-sold-products-by-the-date.sql
+++ 1484-group-sold-products-by-the-date/1484-group-sold-products-by-the-date.sql
@@ -1,11 +1,5 @@
 
-select sell_date,count(distinct product) as num_sold ,group_concat(distinct product) as products
+select sell_date , count(distinct product) as num_sold, GROUP_CONCAT(distinct product separator ',') as products
 from Activities
 group by sell_date
 order by sell_date 
-
-
--- select sell_date,count(distinct  product) as num_sold ,group_concat (distinct product separator ',') as products
--- from Activities
--- group by sell_date
--- order by sell_date
