d301d390770ff8b656cfaaf9d4fba9e16942744d
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2025-07-30 06:18:47 +0530

    Time: 0 ms (100%), Space: 42.2 MB (98%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -1,61 +1,34 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- * int val;
- * ListNode next;
- * ListNode() {}
- * ListNode(int val) { this.val = val; }
- * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        
         ListNode temp = new ListNode(-1);
         ListNode demo = temp;
-
-        while (list1 != null && list2 != null) {
-            if (list1.val > list2.val) {
-                demo.next = list2;
-                list2 = list2.next;
-            } else {
-                demo.next = list1;
-                list1 = list1.next;
+        while(list1!=null && list2!=null ){
+            if(list1.val<list2.val){
+                temp.next=list1;
+                list1=list1.next;
+            }else{
+                temp.next =list2;
+                list2=list2.next;
             }
-            demo = demo.next;
+        temp = temp.next;
 
         }
-
-        if (list1 == null) {
-            demo.next = list2;
+        if(list1==null){
+            temp.next=list2;
+        }if(list2==null){
+            temp.next=list1;
         }
-        if (list2 == null) {
-            demo.next = list1;
-        }
-
-        return temp.next;
-
-//         ListNode temp = new ListNode(-1);
+        return demo.next;
         
-//         while(list1.next!=null && list2.next!=null){
-//             if(list1.val>list2.val){
-//              temp.next =list2;
-//              list2=list2.next;
-//             }else{
-//                 temp.next=list1;
-//                 list1=list1.next;
-//             }
-//         }
-//         if(list1==null){
-//             temp.next=list2;
-
-//         }if(list2==null){
-//             temp.next=list1;
-//         }
-
-        
-      
-//  return temp.next;         
     }
-
 }
