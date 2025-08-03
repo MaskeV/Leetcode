9004fd64b0dd647ceb02ff93d9f1d3c688203609
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2025-08-03 19:53:27 +0530

    Time: 0 ms (100%), Space: 43 MB (8.15%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -8,49 +8,51 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-// class Solution {
-//     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-//         ListNode ans = new ListNode();
-//         ListNode head = ans;
-//         while(list1!=null && list2!=null){
-//             if(list1.val<list2.val){
-//                ans.next=list1;
-//                list1 = list1.next;
-//             }else{
-//                 ans.next = list2;
-//                 list2=list2.next;
-//             }
-//             ans = ans.next;
-//         }
-//         while(list1 !=null){
-//             ans.next=list1;
-//             list1 = list1.next;
-//             ans = ans.next;
-//         }
-//         while(list2!=null){
-//             ans.next = list2;
-//             list2 = list2.next;
-//             ans = ans.next;
-//         }
-//         return head.next;
-
-//     }
-// }
-
-class Solution{
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2){
-        if(l1 == null){
-            return l2;
-        }if(l2 == null){
-            return l1;
-        }if(l1.val<l2.val){
-            l1.next = mergeTwoLists(l1.next,l2);
-            return l1;
-        }else{
-            l2.next = mergeTwoLists(l2.next,l1);
-            return l2;
-
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode ans = new ListNode();
+        ListNode head = ans;
+        while(list1!=null && list2!=null){
+            if(list1.val<list2.val){
+               ans.next=list1;
+               list1 = list1.next;
+            }else{
+                ans.next = list2;
+                list2=list2.next;
+            }
+            ans = ans.next;
         }
-        
+        ans.next= list1!=null ? list1:list2;
+        return head.next;
+        // while(list1 !=null){
+        //     ans.next=list1;
+        //     list1 = list1.next;
+        //     ans = ans.next;
+        // }
+        // while(list2!=null){
+        //     ans.next = list2;
+        //     list2 = list2.next;
+        //     ans = ans.next;
+        // }
+        // return head.next;
+
     }
 }
+
+// class Solution{
+//     public ListNode mergeTwoLists(ListNode l1, ListNode l2){
+//         if(l1 == null){
+//             return l2;
+//         }if(l2 == null){
+//             return l1;
+//         }if(l1.val<l2.val){
+//             l1.next = mergeTwoLists(l1.next,l2);
+//             return l1;
+//         }else{
+//             l2.next = mergeTwoLists(l2.next,l1);
+//             return l2;
+
+//         }
+        
+//     }
+// }
