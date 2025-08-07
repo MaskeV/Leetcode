8ece4be266cc103493c12624808da52c6f5864cc
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2025-08-07 20:19:43 +0530

    Time: 3 ms (91.89%), Space: 43.2 MB (70.67%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -17,19 +17,19 @@ class Solution {
         // }
         // return true;
 
-        int [] alphabate = new int[26];
-        for(int i=0;i<s.length();i++){
-            alphabate[s.charAt(i)-'a']++; 
+      int [] alphabate = new int[26];
+      for(int i=0;i<s.length();i++){
+        alphabate[s.charAt(i)-'a']++;
+      }
+      for(int i=0;i<t.length();i++){
+        alphabate[t.charAt(i)-'a']--;
+      }
+      for(int i:alphabate){
+        if(i!=0){
+            return false;
         }
-        for(int i=0;i<t.length();i++){
-            alphabate[t.charAt(i)-'a']--;
-        }
-        for(int i:alphabate){
-            if(i!=0){
-                return false;
-            }
-        }
-        return true;
+      }
+      return true;
     }
 }
         
