48e0e128c364275ae9c4c211aea4132f19d96555
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2025-08-10 14:51:47 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,15 +1,18 @@
-class Solution {
+// class Solution {
+//     public boolean isPowerOfTwo(int n) {
+//         if(n<=0){
+//             return false;
+//         }
+//         while(n%2==0){
+//             n=n/2;
+//         }
+//         return n==1;
+        
+        
+//     }
+// }
+class Solution{
     public boolean isPowerOfTwo(int n) {
-        if(n<=0){
-            return false;
-        }
-        while(n%2==0){
-            n=n/2;
-        }
-        return n==1;
-        
-        // return n > 0 && (n & (n - 1)) == 0;
-
-        
+        return n>=0 && (n & n-1)==0;
     }
 }
