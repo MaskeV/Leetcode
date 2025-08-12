4b127a9a9a39ea642367bcea1024eb109868770d
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2025-08-12 13:18:32 +0530

    Time: 1 ms (99.84%), Space: 46.1 MB (60.46%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,10 +1,10 @@
 class Solution {
     public int singleNumber(int[] nums) {
         int ans=0;
-        for(int i=0;i<nums.length;i++){
-         ans = ans ^ nums[i];
+        for(int i:nums){
+         ans = ans ^ i;
         }
         return ans;
-        
-    }
+
+    } 
 }
