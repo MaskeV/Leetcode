f8b629f817675bb793dba7bb6ebc148d3b0eb3d3
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2025-08-13 22:15:52 +0530

    Time: 0 ms (100%), Space: 44.4 MB (98.36%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.java
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -1,12 +1,17 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
         int[] rotate = new int[nums.length];
-        int id=0;
+
         for(int i=0;i<n;i++){
-  rotate[id]=nums[i];
-  rotate[id+1]=nums[i+n];
-  id+=2;
+            rotate[2*i]=nums[i];
+            rotate[2*i+1]=nums[i+n];
         }
+//         int id=0;
+//         for(int i=0;i<n;i++){
+//   rotate[id]=nums[i];
+//   rotate[id+1]=nums[i+n];
+//   id+=2;
+//         }
         return rotate;
     }
 }
