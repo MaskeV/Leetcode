6d14a1909f22ca7f6cb235eb6d014b582096326e
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2025-08-14 12:43:48 +0530

    Time: 0 ms (100%), Space: 42.4 MB (19.7%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,34 +1,23 @@
 // 
 class Solution {
     public int search(int[] nums, int target) {
-        int peak=0;
-        int max=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]>max){
-                max=nums[i];
-                peak=i;
-            }
-        }
-        int ans = binarySearch(nums,target,0,peak);
-        if(ans!=-1){
-            return ans;
-        }if(peak<nums.length-1){
-        return binarySearch(nums,target,peak+1,nums.length-1);
-        }
-        return -1;
-
-    }
-
-     //when order is not known 
-     private int binarySearch(int[] nums,int target,int start,int end){
+       int  start=0,end=nums.length-1;
         while(start<=end){
             int mid = start+(end-start)/2;
             if(nums[mid]==target){
                 return mid;
-            }else if(nums[mid]>target){
-                   end=mid-1;
+            }else if(nums[start]<=nums[mid]){
+                if(nums[start]<=target && nums[mid]>=target){
+                  end =mid-1;
+                }else{
+                    start=mid+1;
+                }
             }else{
-                start=mid+1;
+                if(nums[end]>=target && target>=nums[mid]){
+                    start=mid+1;
+                }else{
+                    end=mid-1;
+                }
             }
 
             }
