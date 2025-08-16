c2c33ab7ea8fcfe9de5f4846f9ec77da52e44046
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2025-08-16 07:09:01 +0530

    Time: 1 ms (68.54%), Space: 45.7 MB (13.86%) - LeetHub

--- 0566-reshape-the-matrix/0566-reshape-the-matrix.java
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.java
@@ -1,29 +1,50 @@
-class Solution {
+class Solution{
     public int[][] matrixReshape(int[][] mat, int r, int c) {
-        if(r*c!=(mat.length*mat[0].length)){
-            return mat;
+        int m=mat.length;
+        int n=mat[0].length;
+
+        if(r*c!=m*n){
+          return mat;
         }
-        
 
         int[][] res = new int[r][c];
-       List<Integer> l = new ArrayList<>();
-       for(int i=0;i<mat.length;i++){
-        for(int j=0;j<mat[0].length;j++){
-          l.add(mat[i][j]);
+        for(int i=0;i<m*n;i++){
+           res[i/c][i%c]=mat[i/n][i%n];
         }
-       }
-       int idx=0;
 
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                if(idx<l.size()){
-            res[i][j]= l.get(idx);
-            idx++;
-                }
-            }
-        }
-        
         return res;
-        
+
     }
+
 }
+
+
+// class Solution {
+//     public int[][] matrixReshape(int[][] mat, int r, int c) {
+//         if(r*c!=(mat.length*mat[0].length)){
+//             return mat;
+//         }
+        
+
+//         int[][] res = new int[r][c];
+//        List<Integer> l = new ArrayList<>();
+//        for(int i=0;i<mat.length;i++){
+//         for(int j=0;j<mat[0].length;j++){
+//           l.add(mat[i][j]);
+//         }
+//        }
+//        int idx=0;
+
+//         for(int i=0;i<r;i++){
+//             for(int j=0;j<c;j++){
+//                 if(idx<l.size()){
+//             res[i][j]= l.get(idx);
+//             idx++;
+//                 }
+//             }
+//         }
+        
+//         return res;
+        
+//     }
+// }
