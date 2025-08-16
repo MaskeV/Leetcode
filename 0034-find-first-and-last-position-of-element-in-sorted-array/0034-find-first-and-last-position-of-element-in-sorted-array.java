7030f55fc13bbc5b58d554ce403e56eeb8342f69
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2025-08-16 21:36:48 +0530

    Time: 1 ms (19.06%), Space: 46.3 MB (16.49%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,32 +1,55 @@
+// class Solution {
+//     public int[] searchRange(int[] nums, int target) {
+//         int[] ans = {-1,-1};
+//         int start = binarySearch(nums,target,true);
+//         int end = binarySearch(nums,target,false);
+//         ans[0]=start;
+//         ans[1]=end;
+//         return ans;
+        
+//     }
+//    private int binarySearch(int[] nums, int target ,boolean isSearchingStart){
+//     int i=0,j=nums.length-1;
+//     int ans=-1;
+//         while(i<=j){
+//             int mid = i+(j-i)/2;
+//             if(nums[mid]>target){
+//                 j=mid-1;
+//             }else if(nums[mid]<target){
+//                 i=mid+1;
+//             }else{
+//                 ans=mid;
+//                   if(isSearchingStart){
+//                     j=mid-1;
+//                   }else{
+//                     i=mid+1;
+//                   }
+//                 }
+//             }
+//              return ans;
+//         }    
+// }
+
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int[] ans = {-1,-1};
-        int start = binarySearch(nums,target,true);
-        int end = binarySearch(nums,target,false);
-        ans[0]=start;
-        ans[1]=end;
-        return ans;
-        
-    }
-   private int binarySearch(int[] nums, int target ,boolean isSearchingStart){
-    int i=0,j=nums.length-1;
-    int ans=-1;
-        while(i<=j){
-            int mid = i+(j-i)/2;
-            if(nums[mid]>target){
-                j=mid-1;
-            }else if(nums[mid]<target){
-                i=mid+1;
-            }else{
-                ans=mid;
-                  if(isSearchingStart){
-                    j=mid-1;
-                  }else{
-                    i=mid+1;
-                  }
-                }
-            }
-             return ans;
-        }
+        int[] ans = {-1, -1};
+
        
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                ans[0] = i;
+                break;
+            }
+        }
+
+    
+        for (int j = nums.length - 1; j >= 0; j--) {
+            if (nums[j] == target) {
+                ans[1] = j;
+                break;
+            }
+        }
+
+        return ans;
+    }
 }
