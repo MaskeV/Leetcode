0b2360cf11d6cbe425c1eeddbed404d4661b4125
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2025-08-18 20:17:34 +0530

    Time: 2 ms (96.55%), Space: 41.9 MB (47.63%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,38 +1,16 @@
-// class Solution {
-//     public boolean isValid(String s) {
-//      Stack<Character> stack = new Stack<Character>(); 
-//         for (char c : s.toCharArray()) { 
-//             if (c == '(') 
-//                 stack.push(')');
-//             else if (c == '{') 
-//                 stack.push('}'); 
-//             else if (c == '[') 
-//                 stack.push(']'); 
-//             else if (stack.isEmpty() || stack.pop() != c)
-    
-//                 return false;
-//         }
-  
-//         return stack.isEmpty();
-        
-//     }
-// }
-
 class Solution {
      public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
-        for(char c:s.toCharArray()){
-            if(c=='(')
-            stack.push(')');
-         
-            else if(c=='{')
-            stack.push('}');
-            
-            else if(c=='[')
-            stack.push(']');
-            
-            else if (stack.isEmpty() || stack.pop() != c)
-               return false;
+        for(char ch:s.toCharArray()){
+            if(ch=='{'){
+                stack.push('}');
+            }else if(ch=='['){
+                stack.push(']');
+            }else if(ch=='('){
+                stack.push(')');
+            }else if(stack.isEmpty() || stack.pop()!=ch){
+                   return false;
+            }
         }
         return stack.isEmpty();
         }
