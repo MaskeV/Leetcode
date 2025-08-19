b90aa749caa38a0c30f60c5bf40c1b61e9dda92b
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2025-08-19 05:32:51 +0530

    Time: 0 ms (100%), Space: 41.9 MB (48.72%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.java
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.java
@@ -1,26 +1,45 @@
 class Solution {
     public String reversePrefix(String word, char ch) {
-        if(!word.contains(ch+"")){
-            return word;
-        }
-        int next=word.length()-1;
-        StringBuilder sb = new StringBuilder();
-        int count=0;
-        for(int i=0;i<word.length();i++){
-
-            if(word.charAt(i)==ch && count<1){
-                int id=i;
-                next=i+1;
-                while(id>=0){
-                    sb.append(word.charAt(id));
-                    id--;
-                    count++;
-                }
-            }
-        }
-        
-
-        return sb.toString()+word.substring(next,word.length());
-        
+        char[] chars = word.toCharArray();
+       int idx = word.indexOf(ch);
+       int left=0;
+       int right=idx;
+       while(left<right){
+        char temp = chars[left];
+        chars[left]=chars[right];
+        chars[right]=temp;
+        left++;
+        right--;
+       }
+       return new String(chars);
     }
+
 }
+
+
+// class Solution {
+//     public String reversePrefix(String word, char ch) {
+//         if(!word.contains(ch+"")){
+//             return word;
+//         }
+//         int next=word.length()-1;
+//         StringBuilder sb = new StringBuilder();
+//         int count=0;
+//         for(int i=0;i<word.length();i++){
+
+//             if(word.charAt(i)==ch && count<1){
+//                 int id=i;
+//                 next=i+1;
+//                 while(id>=0){
+//                     sb.append(word.charAt(id));
+//                     id--;
+//                     count++;
+//                 }
+//             }
+//         }
+        
+
+//         return sb.toString()+word.substring(next,word.length());
+        
+//     }
+// }
