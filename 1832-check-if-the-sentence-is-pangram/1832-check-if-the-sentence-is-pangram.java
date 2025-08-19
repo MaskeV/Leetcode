d2d019df67616c1d3b6d1d6025841d64c32e8669
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2025-08-19 06:17:42 +0530

    Time: 2 ms (60.6%), Space: 42 MB (18.21%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -1,30 +1,29 @@
-// class Solution {
-//     public boolean checkIfPangram(String sentence) {
-//         int[] chars = new int[26];
-//         for(char c:sentence.toCharArray()){
-//             chars[c-'a']++;
-//         }
-//         for(int i=0;i<26;i++){
-//          if(chars[i]==0){
-//             return false;
-//          }
-
-//         }
-//         return true;
-        
-//     }
-// }
-
-class Solution{
+class Solution {
     public boolean checkIfPangram(String sentence) {
-        Set<Character> set = new HashSet<>();
-        for(char c: sentence.toCharArray()){
-            set.add(c);
-        }
-        if(set.size()!=26){
+    int[] chars = new int[26];
+    for(int i=0;i<sentence.length();i++){
+        chars[sentence.charAt(i)-'a']++;
+    }
+    for(int i=0;i<26;i++){
+        if(chars[i]==0){
             return false;
         }
-        return true;
     }
-
+    return true;
+        
+    }
 }
+
+// class Solution{
+//     public boolean checkIfPangram(String sentence) {
+//         Set<Character> set = new HashSet<>();
+//         for(char c: sentence.toCharArray()){
+//             set.add(c);
+//         }
+//         if(set.size()!=26){
+//             return false;
+//         }
+//         return true;
+//     }
+
+// }
