c04574ae5da4d386466f9b68db4d477a091ede33
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2025-08-20 16:29:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -0,0 +1,38 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        int start = 0;
+        int end = 0;
+        int max = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i; j < s.length() - 1; j++) {
+                if (palindrome(s.substring(i, j + 1))) {
+                    if ((j - i + 1) > max) {
+                        max = Math.max(max, (j - i + 1));
+                        start = i;
+                        end = j;
+
+                    }
+                }
+
+            }
+        }
+        return s.substring(start, end + 1);
+
+    }
+
+    boolean palindrome(String s) {
+        int start = 0;
+        int end = s.length() - 1;
+        while (start < end) {
+            if(s.charAt(start)!=s.charAt(end)){
+                return false;
+            }
+            start++;
+            end--;
+        }
+        
+        return true;
+
+    }
+}
