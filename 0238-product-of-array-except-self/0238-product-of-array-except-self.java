092ae3039516c595fd1e8602c018f2c1ce6a39e1
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2025-08-21 21:03:20 +0530

    Time: 2 ms (87.16%), Space: 55.2 MB (87.7%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,16 +1,33 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-     int[] res = new int[nums.length];
-     Arrays.fill(res,1);
-     int l=1,r=1;
-     for(int i=0;i<nums.length;i++){
-        res[i]=res[i]*l;
-        l=l*nums[i];
-     }
-     for(int i=nums.length-1;i>=0;i--){
-        res[i]=res[i]*r;
-        r=r*nums[i];
-     }
-     return res;
+        int[] res = new int[nums.length];
+        Arrays.fill(res,1);
+        int l=1,r=1;
+        for(int i=0;i<nums.length;i++){
+            res[i]=res[i]*l;
+            l=l*nums[i];
+        }
+        for(int i=nums.length-1;i>=0;i--){
+            res[i]=res[i]*r;
+            r=r*nums[i];
+        }
+        return res;
     }
 }
+
+// class Solution {
+//     public int[] productExceptSelf(int[] nums) {
+//      int[] res = new int[nums.length];
+//      Arrays.fill(res,1);
+//      int l=1,r=1;
+//      for(int i=0;i<nums.length;i++){
+//         res[i]=res[i]*l;
+//         l=l*nums[i];
+//      }
+//      for(int i=nums.length-1;i>=0;i--){
+//         res[i]=res[i]*r;
+//         r=r*nums[i];
+//      }
+//      return res;
+//     }
+// }
