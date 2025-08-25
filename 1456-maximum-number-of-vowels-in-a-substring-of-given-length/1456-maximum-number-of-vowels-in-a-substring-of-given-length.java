140c5571aa121a4630e71135ceacf752d1b17ca0
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2025-08-25 19:55:24 +0530

    Time: 12 ms (82.45%), Space: 44.7 MB (55.5%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -1,24 +1,52 @@
+// class Solution {
+//     public int maxVowels(String s, int k) {
+//       int left=0;
+//       int vowel=0;
+//       int max=0;
+//       for(int right=0;right<s.length();right++){
+//         if(isVowel(s.charAt(right))){
+//             vowel++;
+//         }
+//         if((right-left+1)==k){
+//             max = Math.max(max,vowel);
+//             if(isVowel(s.charAt(left))){
+//                 vowel--;
+//             }
+//             left++;
+//         }
+//       }
+//         return max;
+//       }
+//         boolean isVowel(char c){
+//         return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+//     }
+    
+// }
+
+
 class Solution {
     public int maxVowels(String s, int k) {
-      int left=0;
-      int vowel=0;
-      int max=0;
-      for(int right=0;right<s.length();right++){
-        if(isVowel(s.charAt(right))){
-            vowel++;
+        int max=0;
+        int vowel=0;
+        for(int i=0;i<k;i++){
+            if(isVowel(s.charAt(i))){
+                vowel++;
+            }
         }
-        if((right-left+1)==k){
-            max = Math.max(max,vowel);
-            if(isVowel(s.charAt(left))){
+        max = vowel;
+        for(int i=k;i<s.length();i++){
+           
+            if(isVowel(s.charAt(i))){
+                vowel++;
+            }if(isVowel(s.charAt(i-k))){
                 vowel--;
             }
-            left++;
+
+            max = Math.max(max,vowel);
         }
-      }
         return max;
-      }
-        boolean isVowel(char c){
-        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
     }
-    
+    boolean isVowel (char c){
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o'  || c == 'u'; 
+    }
 }
