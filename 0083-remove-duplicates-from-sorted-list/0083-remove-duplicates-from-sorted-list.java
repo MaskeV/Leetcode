e633e1c84d926a3b167604766b18f4c88944a46a
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2025-08-25 23:22:54 +0530

    Time: 0 ms (100%), Space: 43.9 MB (87.27%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
@@ -14,8 +14,9 @@ class Solution {
       while(temp!=null && temp.next!=null){
         if(temp.val == temp.next.val){
             temp.next = temp.next.next;
-        }
+        }else{
         temp = temp.next;
+        }
       }
       return head;
     }
