296e6eaf78e1962ffda119549ece5f41cbc30559
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2025-08-27 21:00:37 +0530

    Time: 4 ms (97.79%), Space: 64.1 MB (39.38%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.java
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.java
@@ -8,22 +8,53 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// class Solution {
+//     public int pairSum(ListNode head) {
+//         ListNode mover = head;
+//         List<Integer> arr = new ArrayList<>();
+//         while(mover!=null){
+//             arr.add(mover.val);
+//             mover = mover.next;
+//         }
+//         int max=0;
+//         int n = arr.size();
+
+//         for(int i=0;i<n/2;i++){
+//             int sum = arr.get(i) + arr.get(n-1-i);
+//             max = Math.max(sum,max);   
+//         }
+        
+//         return max;
+//     }
+// }
+
 class Solution {
     public int pairSum(ListNode head) {
-        ListNode mover = head;
-        List<Integer> arr = new ArrayList<>();
-        while(mover!=null){
-            arr.add(mover.val);
-            mover = mover.next;
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        ListNode mid = slow;
+        ListNode prev = null;
+        ListNode pres=mid;
+        ListNode next = mid.next;
+        while(pres!=null){
+            pres.next =prev;
+            prev=pres;
+            pres = next;
+            if(next!=null){
+                next = next.next;
+            }
         }
         int max=0;
-        int n = arr.size();
-
-        for(int i=0;i<n/2;i++){
-            int sum = arr.get(i) + arr.get(n-1-i);
-            max = Math.max(sum,max);   
+        while(prev!=null){
+            int sum = head.val + prev.val;
+            max = Math.max(sum,max);
+            head = head.next;
+            prev = prev.next;
         }
-        
         return max;
     }
 }
