14ed3ecadde5b8fc3a411185b10cf9aecda25b85
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2025-08-31 23:50:53 +0530

    Time: 14 ms (14.85%), Space: 45 MB (23.46%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,16 +1,42 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-         Set seen = new HashSet();
-    for (int i=0; i<9; ++i) {
-        for (int j=0; j<9; ++j) {
-            char number = board[i][j];
-            if (number != '.')
-                if (!seen.add(number + " in row " + i) ||
-                    !seen.add(number + " in column " + j) ||
-                    !seen.add(number + " in block " + i/3 + "-" + j/3))
-                    return false;
+        // Set<Integer> row = new HashSet<>();
+        // Set<Integer> column = new HashSet<>();
+        // Set<Integer> block = new HashSet<>();
+        Set set = new HashSet<>();
+
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                char num=board[i][j];
+                if(num!='.'){
+                    if(!set.add(num+"in row"+i) || !set.add(num+"in column"+j)|| !set.add(num +"in block"+ i/3 + "-" + j/3)){
+                        return false;
+                    }
+                }
+            }
         }
-    }
-    return true;
+        return true;
+
     }
 }
+
+
+
+
+
+// class Solution {
+//     public boolean isValidSudoku(char[][] board) {
+//          Set seen = new HashSet();
+//     for (int i=0; i<9; ++i) {
+//         for (int j=0; j<9; ++j) {
+//             char number = board[i][j];
+//             if (number != '.')
+//                 if (!seen.add(number + " in row " + i) ||
+//                     !seen.add(number + " in column " + j) ||
+//                     !seen.add(number + " in block " + i/3 + "-" + j/3))
+//                     return false;
+//         }
+//     }
+//     return true;
+//     }
+// }
