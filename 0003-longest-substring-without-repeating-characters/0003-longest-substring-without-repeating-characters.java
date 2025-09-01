347d05e4de765945d083035f38035548103c2f21
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2025-09-01 12:02:58 +0530

    Time: 7 ms (34.4%), Space: 44.8 MB (43.94%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,26 +1,43 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int max =0;
         int left =0;
-        for(int right = left;right<=s.length();right++){
-            if(isUnique(s.substring(left,right))){
-                max = Math.max(max,right-left);
-            }else{
-                left++;
-            }
+        int max=0;
+        Set<Character> set = new HashSet<>();
+        for(int right=0;right<s.length();right++){
+          while(set.contains(s.charAt(right))){
+            set.remove(s.charAt(left));
+            left++;
+          }
+          set.add(s.charAt(right));
+          max = Math.max(max,right-left+1);
         }
         return max;
     }
-    boolean isUnique(String s){
-       Map<Character,Integer> map = new HashMap<>();
-       for(char c: s.toCharArray()){
-        map.put(c,map.getOrDefault(c,0)+1);
-       }
-        for(char c: s.toCharArray()){
-        if(map.get(c)>1){
-            return false;
-        }
-       }
-       return true;
-    }
 }
+
+// class Solution {
+//     public int lengthOfLongestSubstring(String s) {
+//         int max =0;
+//         int left =0;
+//         for(int right = left;right<=s.length();right++){
+//             if(isUnique(s.substring(left,right))){
+//                 max = Math.max(max,right-left);
+//             }else{
+//                 left++;
+//             }
+//         }
+//         return max;
+//     }
+//     boolean isUnique(String s){
+//        Map<Character,Integer> map = new HashMap<>();
+//        for(char c: s.toCharArray()){
+//         map.put(c,map.getOrDefault(c,0)+1);
+//        }
+//         for(char c: s.toCharArray()){
+//         if(map.get(c)>1){
+//             return false;
+//         }
+//        }
+//        return true;
+//     }
+// }
