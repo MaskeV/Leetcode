260f1a7c387551021801916aab15bb46f2928183
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2025-09-02 13:33:07 +0530

    Time: 0 ms (100%), Space: 41.6 MB (88.9%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -8,20 +8,46 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// class Solution {
+//     public ListNode removeNthFromEnd(ListNode head, int n) {
+//         ListNode res = new ListNode(0,head);
+//         ListNode dummy = res;
+
+//         for(int i=0;i<n;i++){
+//             head=head.next;
+//         }
+
+//         while(head!=null){
+//             head=head.next;
+//             dummy = dummy.next;
+//         }
+//        dummy.next=dummy.next.next;
+//        return res.next;
+//     }
+// }
+
+
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode res = new ListNode(0,head);
-        ListNode dummy = res;
-
-        for(int i=0;i<n;i++){
-            head=head.next;
+        int length=0;
+        ListNode temp = head;
+        while(temp!=null){
+            length++;
+            temp=temp.next;
         }
-
-        while(head!=null){
-            head=head.next;
-            dummy = dummy.next;
+        temp=head;
+         if(length==1){
+            return null;
+        }if(length==n){
+            return head.next;
         }
-       dummy.next=dummy.next.next;
-       return res.next;
+        for(int i=0;i<length-n-1;i++){
+         temp=temp.next;
+        }
+        if(temp.next!=null){
+        temp.next=temp.next.next;
+        }
+       
+        return head;
     }
 }
