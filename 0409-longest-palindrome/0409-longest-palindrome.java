1c5c3d4386f2bd4261403184088943328b80c1ea
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2025-09-03 20:18:53 +0530

    Time: 8 ms (29.55%), Space: 42.3 MB (21.23%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.java
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -1,23 +1,22 @@
 class Solution {
     public int longestPalindrome(String s) {
-        Map<Character,Integer> map = new HashMap<>();
-        for(int i=0;i<s.length();i++){
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            map.put(c,map.getOrDefault(c,0)+1);
+            map.put(c, map.getOrDefault(c, 0) + 1);
         }
-        int count =0;
-        boolean flag = true;
+        int count = 0;
+        for (char c : map.keySet()) {
 
-        for(char c:map.keySet()){
-         
-            if(map.get(c)%2==0){
-                count+=map.get(c);
-            }if(map.get(c)%2!=0){
-                count+=map.get(c)-1;
-                flag=false;
+            if (map.get(c) % 2 == 0) {
+                count += map.get(c);
             }
-        }if(count<s.length()){
-        count++;
+            if (map.get(c) % 2 != 0) {
+                count += map.get(c) - 1;
+            }
+        }
+        if (count < s.length()) {
+            count++;
         }
         return count;
     }
