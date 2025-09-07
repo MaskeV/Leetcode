a2c40599a9bd2b794ebe365d0be7a7fc7483a0ba
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2025-09-07 19:24:55 +0530

    Time: 25 ms (22.5%), Space: 59.8 MB (44.19%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -13,20 +13,19 @@
 //  }
   class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        if(k==0){
-            return false;
-        }
+        
         HashSet<Integer> set = new HashSet<>();
         int j=0;
         for(int i=0;i<nums.length;i++){
-            if(!set.add(nums[i])){
+            if(set.contains(nums[i])){
                return true;
             }
+              set.add(nums[i]);
             if(set.size()>k){
-              set.remove(nums[j]);
+              set.remove(nums[i-k]);
               j++;
             }
-            set.add(nums[i]);
+          
         } 
         return false;
     }
