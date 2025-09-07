987a1528d9b9a23e0320d57998efc2a9e23cff3c
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2025-09-07 21:05:47 +0530

    Time: 17 ms (35.01%), Space: 44.8 MB (99.6%) - LeetHub

--- 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.java
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.java
@@ -1,22 +1,43 @@
+// class Solution {
+//     public int findLHS(int[] nums) {
+//         int max=0;
+//         for(int i=0;i<nums.length;i++){
+//            int count =0;
+//            boolean flag=false;
+//             for(int j=0;j<nums.length;j++){
+//                 if(nums[j]-nums[i]==1  || nums[i]==nums[j]){
+//                     count++;
+//                     if(nums[j]-nums[i]==1){
+//                         flag = true;
+//                     }
+//                 }
+//             }
+//             if(flag){
+//             max= Math.max(count,max);
+//             }
+//         }
+//         return max;
+        
+//     }
+// }
+
 class Solution {
     public int findLHS(int[] nums) {
+        int sum=0;
         int max=0;
-        for(int i=0;i<nums.length;i++){
-           int count =0;
-           boolean flag=false;
-            for(int j=0;j<nums.length;j++){
-                if(nums[j]-nums[i]==1  || nums[i]==nums[j]){
-                    count++;
-                    if(nums[j]-nums[i]==1){
-                        flag = true;
-                    }
-                }
-            }
-            if(flag){
-            max= Math.max(count,max);
-            }
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i:nums){
+            map.put(i,map.getOrDefault(i,0)+1);
         }
-        return max;
-        
+       
+    for(int i:map.keySet()){
+        if(map.keySet().contains(i+1)){
+          sum = map.get(i)+map.get(i+1);
+        }
+        max=Math.max(max,sum);
+    }
+    return max;
     }
 }
+
+
