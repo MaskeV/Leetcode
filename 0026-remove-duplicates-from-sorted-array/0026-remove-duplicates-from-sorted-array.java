5953bd99330625ec8d20417f036d1e40e3065006
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2025-09-08 19:23:37 +0530

    Time: 1 ms (59.21%), Space: 45 MB (41.46%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,13 +1,30 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-     int i=1;
-
-    for(int j=1;j<nums.length;j++){
-        if(nums[j]!=nums[i-1]){
-            nums[i]=nums[j];
-            i++;
+        int n=nums.length;
+        int j=1;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]!=nums[j-1]){
+                nums[j]=nums[i];
+                j++;
+            }
         }
-    }
-    return i;
+        return j;
     }
 }
+// class Solution {
+//     public int removeDuplicates(int[] nums) {
+//         int n=nums.length;
+//         if(n==1){
+//             return n;
+//         }
+//      int i=1;
+
+//     for(int j=1;j<n;j++){
+//         if(nums[j]!=nums[i-1]){
+//             nums[i]=nums[j];
+//             i++;
+//         }
+//     }
+//     return i;
+//     }
+// }
