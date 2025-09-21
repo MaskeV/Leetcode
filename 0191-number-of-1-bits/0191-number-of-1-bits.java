3d9cf22d84492f7dfd366943b4e82c58550477ca
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2025-09-21 23:08:53 +0530

    Time: 1 ms (16.83%), Space: 40.7 MB (72.92%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.java
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -2,10 +2,12 @@ class Solution {
     public int hammingWeight(int n) {
         int count=0;
         while(n>0){
-        if(n%2!=0){
-           count++;
-        }
-            n=n/2;
+        // if(n%2!=0){
+        //    count++;
+        // }
+        //     n=n/2;
+        n=n & (n-1);
+        count++;
         }
         return count;
     }
