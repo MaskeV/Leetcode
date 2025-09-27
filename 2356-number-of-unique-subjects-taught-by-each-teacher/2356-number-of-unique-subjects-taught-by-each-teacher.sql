e3058f171d9665bbd46a2ee526098d20f839bfde
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2025-09-27 10:42:17 +0530

    Time: 554 ms (29.55%), Space: 0B (100%) - LeetHub

--- 2356-number-of-unique-subjects-taught-by-each-teacher/2356-number-of-unique-subjects-taught-by-each-teacher.sql
+++ 2356-number-of-unique-subjects-taught-by-each-teacher/2356-number-of-unique-subjects-taught-by-each-teacher.sql
@@ -7,10 +7,10 @@
 -- from (select teacher_id,subject_id from Teacher group by teacher_id,subject_id) as T
 -- group by teacher_id;
 
-
-select teacher_id ,count(distinct subject_id) as cnt
-from Teacher
-group by teacher_id
+ 
+select teacher_id, count(distinct subject_id) as cnt 
+from Teacher 
+group by teacher_Id;
 
 
 
