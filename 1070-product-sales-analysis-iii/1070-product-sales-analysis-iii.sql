fc456751f23ab8c872e7f01580bb2cc86764f817
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2025-09-27 11:01:19 +0530

    Time: 680 ms (76.94%), Space: 0B (100%) - LeetHub

--- 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -3,4 +3,4 @@
 
 select product_id,year as first_year, quantity, price
 from Sales
-where( year) in (select   min(year) from Sales group by product_id)
+where(product_id, year) in (select  product_id, min(year) from Sales group by product_id)
