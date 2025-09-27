d9557e782e5fa5334a0b62a2195ee7dd10b57e10
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2025-09-27 13:26:40 +0530

    Time: 1741 ms (15.07%), Space: 0B (100%) - LeetHub

--- 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -7,11 +7,10 @@
 -- limit 1
 
 
-select person_name
-from(
-    select person_name,weight , turn,
-    sum(weight) over (order by turn) as total_weight  from Queue
-) as subquerry where total_weight <= 1000
-order by turn desc
+select q1.person_name
+from Queue q1 join Queue q2
+on q1.turn>=q2.turn
+group by q1.turn
+having sum(q2.weight)<=1000
+order by sum(q2.weight) desc
 limit 1
-
