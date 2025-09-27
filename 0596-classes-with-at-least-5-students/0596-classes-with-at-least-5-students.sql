6b57b91a4fc38bff4a0c5113c7f94b203af27a0d
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2025-09-27 11:03:43 +0530

    Time: 763 ms (5.01%), Space: 0B (100%) - LeetHub

--- 0596-classes-with-at-least-5-students/0596-classes-with-at-least-5-students.sql
+++ 0596-classes-with-at-least-5-students/0596-classes-with-at-least-5-students.sql
@@ -1,20 +1,9 @@
-# Write your MySQL query statement below
--- SELECT class
--- FROM Courses
--- GROUP BY class
--- having count(student)>=5;
-
-
 -- select class
 -- from (select class,count(student) as  count from Courses group by class) as T
 -- where count>=5;
 
 
-
-
-
-
-select class
+select class 
 from Courses
 group by class
 having count(student)>=5
