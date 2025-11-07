bd0f9f4cc251a2215ed92dbb3be9afb8ab4ac290
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2025-11-07 09:34:40 +0530

    Time: 1324 ms (31.46%), Space: 0B (100%) - LeetHub

--- 1341-movie-rating/1341-movie-rating.sql
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -14,20 +14,19 @@
 -- order by avg(rating) desc , title asc 
 -- limit 1
 
-
-(select name as results
-from Users u join MovieRating M 
-on U.user_id=M.user_id
+(Select name as results
+from MovieRating m join Users U 
+on M.user_id=U.user_id 
 group by M.user_id
-order by count(M.user_id) desc,name
-limit 1)
+order by count(rating) desc , name asc
+limit 1 )
 
-Union all
+union all
 
-(select title
-from Movies M join MovieRating MR
-on M.movie_id = MR.movie_id
-where created_at between '2020-02-01' and '2020-02-29'
-group by M.movie_id
-order by avg(rating) desc,title
+(select title as Results 
+from MovieRating MR join Movies M
+on Mr.movie_id = M.movie_id
+where month(created_at)='02' and year(created_at)='2020'
+group by MR.movie_id
+order by avg(rating) desc , title asc
 limit 1)
