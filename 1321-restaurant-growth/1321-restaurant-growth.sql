c09bf0d775b1412b9d5800d22fb9910563de2135
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2025-11-07 16:15:24 +0530

    Time: 354 ms (36.45%), Space: 0B (100%) - LeetHub

--- 1321-restaurant-growth/1321-restaurant-growth.sql
+++ 1321-restaurant-growth/1321-restaurant-growth.sql
@@ -1,7 +1,7 @@
 # Write your MySQL query statement below
 select visited_on,(select sum(amount) from Customer 
 where visited_on 
-between date_sub(C.visited_on,Interval 6 day) and C.visited_on) as amount, 
+between date_sub(C.visited_on,Interval 6 day) and C.visited_on) as amount ,
 (select round(sum(amount)/7,2) from Customer
 where visited_on
 between date_sub(C.visited_on, Interval 6  day) and C.visited_on) as average_amount
@@ -10,16 +10,4 @@ where visited_on>= (
 select date_add(min(visited_on),Interval 6 day) 
 from Customer)
 group by visited_on
-
--- select visited_on,(select sum(amount) from Customer 
--- where visited_on 
--- between date_sub(C.visited_on,Interval 6 day) and C.visited_on) as amount, 
--- (select round(avg(amount),2) from Customer
--- where visited_on
--- between date_sub(C.visited_on, Interval 6  day) and C.visited_on) as average_amount
--- from Customer C
--- where visited_on>= (
--- select date_add(min(visited_on),Interval 6 day) 
--- from Customer)
--- group by visited_on
 
