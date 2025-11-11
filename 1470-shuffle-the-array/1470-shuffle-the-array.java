a84ae7d507a837508682629adbbada978554ac98
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2025-11-11 14:36:54 +0530

    Time: 0 ms (100%), Space: 46.3 MB (8.92%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.java
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -1,33 +1,13 @@
-// class Solution {
-//     public int[] shuffle(int[] nums, int n) {
-//         int[] rotate = new int[nums.length];
-
-//         for(int i=0;i<n;i++){
-//             rotate[2*i]=nums[i];
-//             rotate[2*i+1]=nums[i+n];
-//         }
-// //         int id=0;
-// //         for(int i=0;i<n;i++){
-// //   rotate[id]=nums[i];
-// //   rotate[id+1]=nums[i+n];
-// //   id+=2;
-// //         }
-//         return rotate;
-//     }
-// }
-
 class Solution {
     public int[] shuffle(int[] nums, int n) {
-        int maxVal = 10000; // larger than any number in nums
-        // Step 1: Encode
-        for (int i = 0; i < n; i++) {
-            nums[i] = nums[i] + nums[i + n] * maxVal;
+        int[] ans = new int[2*n];
+        int j=0;
+
+        for(int i=0;i<n;i++){
+            ans[j++]=nums[i];
+            ans[j++]=nums[i+n];
         }
-        // Step 2: Decode in reverse order to avoid overwriting
-        for (int i = n - 1; i >= 0; i--) {
-            nums[2 * i + 1] = nums[i] / maxVal; // y
-            nums[2 * i] = nums[i] % maxVal;     // x
-        }
-        return nums;
+        return ans;
+        
     }
 }
