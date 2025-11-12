5e19fdf5f50851924d678bfcab2151a76e84c4ba
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2025-11-12 21:47:51 +0530

    Time: 1 ms (83.93%), Space: 44 MB (5.29%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.java
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.java
@@ -1,40 +1,16 @@
 class Solution {
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
-    //     List<Boolean> ans = new ArrayList<>();
-    //     int max =0;
-    //     for(int i=0;i<candies.length;i++){
-    //         if(candies[i]>max){
-    //             max=candies[i];
-    //         }
-    //     }
-    //     for(int i=0;i<candies.length;i++){
-    //         if(candies[i]+extraCandies >= max){
-    //           ans.add(true);  
-
-    //         }else{
-    //             ans.add(false);
-    //         }
-    //     }
-    //     return ans;
-    // }
-
-    List<Boolean> ans = new ArrayList<>();
-    int max =candies[0];
-    for(int i=0;i<candies.length;i++){
+        int max=0;
+        List<Boolean> flags = new ArrayList<>();
+        for(int i=0;i<candies.length;i++){
         if(max<candies[i]){
-            max=candies[i];
+          max=candies[i];
         }
-    }
-    for(int i:candies){
-        int total = i+extraCandies;
-        if(total<max){
-            ans.add(false);
-        } else{
-            ans.add(true);
-        }
-    }
+       }
 
-    return ans;
-
+       for(int i=0;i<candies.length;i++){ 
+           flags.add(candies[i]+extraCandies>=max);
+       } 
+       return flags;
     }
 }
