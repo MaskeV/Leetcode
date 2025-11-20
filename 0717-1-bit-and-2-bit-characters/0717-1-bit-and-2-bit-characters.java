1e975458692405ca10c548922eb1b5360ab25b5f
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2025-11-20 21:49:53 +0530

    Time: 0 ms (100%), Space: 44.6 MB (12.19%) - LeetHub

--- 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.java
+++ 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.java
@@ -1,15 +1,10 @@
 class Solution {
     public boolean isOneBitCharacter(int[] bits) {
         int n= bits.length;
-        if(n==1){
-            return true;
-        }
-        if(n%2==1){
-            return true;
-        }
-        if(bits[n-2]==0){
-            return true;
-        }
-        return false;
+        int i=0;
+         while(i<n-1){
+        i+=1+bits[i];
+       }
+       return i==n-1;
     }
 }
