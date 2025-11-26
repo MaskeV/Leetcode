1f7546a4e36c84346684bf5f2e56f08845cac465
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2025-11-26 16:44:27 +0530

    Time: 25 ms (42.84%), Space: 40.8 MB (47.94%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -4,17 +4,15 @@
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
         int start=0,end=n;
-        while(start<=end){
+        while(start<end){
             int mid=start+(end-start)/2;
             boolean bad = isBadVersion(mid);
-            if(bad==false){
-             start=mid+1;
-            }else if(isBadVersion(mid-1)){
-                end=mid-1;
+            if(bad){
+                end=mid;
             }else{
-                return mid;
+                start=mid+1;
             }
         }
-        return -1;
+        return start;
     }
 }
