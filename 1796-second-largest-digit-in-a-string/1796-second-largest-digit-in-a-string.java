4186b05bb4b59d1a436b4cf2445781d4cfa032e6
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2025-12-08 22:01:50 +0530

    Time: 1 ms (96.81%), Space: 43.5 MB (25.73%) - LeetHub

--- 1796-second-largest-digit-in-a-string/1796-second-largest-digit-in-a-string.java
+++ 1796-second-largest-digit-in-a-string/1796-second-largest-digit-in-a-string.java
@@ -1,15 +1,32 @@
 class Solution {
     public int secondHighest(String s) {
-        TreeSet<Integer> set=new TreeSet<>();
-        for(int i=0;i<s.length();i++){
-           if(s.charAt(i)>='0'&&s.charAt(i)<='9'){
-            set.add(s.charAt(i)-'0');
-           }
+        //TreeSet<Integer> set=new TreeSet<>();
+        //for(int i=0;i<s.length();i++){
+          // if(s.charAt(i)>='0'&&s.charAt(i)<='9'){
+            //set.add(s.charAt(i)-'0');
+           //}
+        //}
+      //if(set.size()<=1){
+        //return -1;
+     // }
+      //set.pollLast();
+      //return set.last();
+
+
+      int last=-1;
+      int secLast=-1;
+
+      for(char c:s.toCharArray()){
+        if(c>='0' && c<='9'){
+            int d=c-'0';
+          if(d>last){
+            secLast=last;
+            last=d;
+          }else if(d<last && d>secLast){
+            secLast=d;
+          }
         }
-      if(set.size()<=1){
-        return -1;
       }
-      set.pollLast();
-      return set.last();
+      return secLast;
     }
 }
