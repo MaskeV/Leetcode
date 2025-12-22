10294c2a721c12661cf6d5b15674e618914bc2d8
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2025-12-22 22:06:30 +0530

    Time: 1 ms (93.2%), Space: 43.7 MB (7.72%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,50 +1,24 @@
 class Solution {
     public List<List<Integer>> generate(int n) {
-        List<List<Integer>> pascal = new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
+
         for(int i=0;i<n;i++){
-            List<Integer> row = new ArrayList<>();
+            List<Integer> temp=new ArrayList<>();
           for(int j=0;j<=i;j++){
-             if(j==0 || j==i){
-                row.add(1);
+            if(n==0){
+                temp.add(1);
+            }
+            else if(j==0|| j==i){
+                temp.add(1);
              }else{
-             row.add(pascal.get(i-1).get(j-1)+pascal.get(i-1).get(j));
+                temp.add(ans.get(i-1).get(j-1)+ans.get(i-1).get(j));
              }
           }
-    pascal.add(row);
+          ans.add(temp);
         }
+      return ans;
+       
 
-        return pascal;
+
     }
 }
-
-// class Solution {
-//     public List<List<Integer>> generate(int numRows) {
-//         List<List<Integer>> pascal = new ArrayList<>();
-//         for(int i=0;i<numRows;i++){
-//             pascal.add(pascal(i));
-//         }
-//         return pascal;
-//     }  
-//     List<Integer> pascal(int n){
-//         List<Integer> temp= new ArrayList<>(1);
-//         if(n==0){
-//             temp.add(1);
-//         }
-//          else if(n==1){
-//             temp.add(1);
-//             temp.add(1);
-//         }
-//         else{
-//             temp.add(1);
-//             List<Integer> t = pascal(n-1);
-//             for(int i=0;i<t.size()-1;i++){
-//                 temp.add(t.get(i)+t.get(i+1));
-//             }
-//             temp.add(1);
-//         }
-     
-
-//         return temp;
-//     }
-    
-// }
