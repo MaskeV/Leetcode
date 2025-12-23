55f459c282da2615a1792d6da9bc16f6bac8c31c
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2025-12-23 12:31:15 +0530

    Time: 1 ms (62.97%), Space: 43.1 MB (69.93%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -7,15 +7,34 @@ class Solution {
          // sb.append(strs[0].charAt(i));
           //i++;
        // }
-       String first=strs[0];
-       String last =strs[strs.length-1];
-       for(int i=0;i<strs[0].length();i++){
-        if(first.charAt(i)==last.charAt(i)){
-            sb.append(first.charAt(i));
-        }else{
-            break;
+       //String first=strs[0];
+       //String last =strs[strs.length-1];
+       //for(int i=0;i<strs[0].length();i++){
+        //if(first.charAt(i)==last.charAt(i)){
+          //  sb.append(first.charAt(i));
+        //}else{
+          //  break;
+        //}
+      // }
+     //   return sb.toString();
+     String prefix= strs[0];
+     int preflen = strs[0].length();
+
+     for(int i=1;i<strs.length;i++){
+        if(strs.length==0 || strs[0].length()==0){
+            return "";
+        }
+        String s= strs[i];
+       while(preflen>s.length() || !prefix.equals(s.substring(0,preflen))){
+        preflen--;
+        prefix=prefix.substring(0,preflen);
+        if(preflen==0){
+            return "";
         }
        }
-        return sb.toString();
+      
+     } 
+
+     return prefix;
     }
 }
