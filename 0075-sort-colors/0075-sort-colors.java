7810fef66da1621bdc37a025b52dc8a64d369fd3
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2025-12-23 10:00:42 +0530

    Time: 0 ms (100%), Space: 43 MB (84.93%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -11,16 +11,35 @@ class Solution {
               //  id++;
            // }
         //}
-        int count[]=new int[3];
-        for(int i=0;i<nums.length;i++){
-           count[nums[i]]++;
-        }
-        int id=0;
-        for(int i=0;i<3;i++){
-            for(int j=0;j<count[i];j++){
-                 nums[id]=i;
-                 id++;
+        //int count[]=new int[3];
+        //for(int i=0;i<nums.length;i++){
+         //  count[nums[i]]++;
+        //}
+        //int id=0;
+        //for(int i=0;i<3;i++){
+          //  for(int j=0;j<count[i];j++){
+            //     nums[id]=i;
+              //   id++;
+            //}
+        //}
+
+        int low=0,high=nums.length-1;
+         int mid=0;
+        while(mid<=high){
+
+            if(nums[mid]==0){           
+            nums[mid]=nums[low];
+            nums[low]=0;
+            mid++;
+            low++;
+            }else if(nums[mid]==2){
+                nums[mid]=nums[high];
+                nums[high]=2;
+                high--;
+            }else{
+               mid++;
             }
         }
+       
     }
 }
