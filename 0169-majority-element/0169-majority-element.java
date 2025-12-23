0a89460487665370fac51bafaaa02af7d9b7a6f7
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2025-12-23 12:33:41 +0530

    Time: 7 ms (44%), Space: 56.1 MB (10.81%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,38 +1,7 @@
 class Solution {
     public int majorityElement(int[] nums) {
-    //   Arrays.sort(nums);
-       int m=nums.length/2;
-    //    return nums[m];
-
-
-    // int count=0;
-    // int candidate = 0;
-
-    // for(int i: nums){
-    //     if(count==0){
-    //         candidate=i;
-    //     }
-    //      else if(i==candidate){
-    //         count++;
-    //     }
-    //     count--;
-    // }
-    // return candidate;
-    
-    int count=0;
-    int no=0;
-    for(int i:nums){
-       if(count==0){
-       no=i;
-       }
-      else if(i==no){
-        count++;
-       }else{
-        count--;
-       }
-
-
-    }
-    return no;
+    Arrays.sort(nums);
+    return nums[nums.length/2];
+        
     }
 }
