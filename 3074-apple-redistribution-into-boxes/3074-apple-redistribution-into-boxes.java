4aac51d1897d78745703bef80f1754a154cdda16
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2025-12-24 23:10:53 +0530

    Time: 1 ms (98.81%), Space: 43.9 MB (89.02%) - LeetHub

--- 3074-apple-redistribution-into-boxes/3074-apple-redistribution-into-boxes.java
+++ 3074-apple-redistribution-into-boxes/3074-apple-redistribution-into-boxes.java
@@ -6,13 +6,29 @@ class Solution {
             appleCount+=apple[i];
         }
         int sum=0;
-        Arrays.sort(capacity);
-        for(int i=capacity.length-1;i>0;i--){
-            sum+=capacity[i];
-            count++;
-            if(sum>=appleCount){
+        //Arrays.sort(capacity);
+        //for(int i=capacity.length-1;i>0;i--){
+         //   sum+=capacity[i];
+           // count++;
+            //if(sum>=appleCount){
+              //  return count;
+            //}
+
+         int freq[]=new int[51];
+         for(int i=0;i<capacity.length;i++){
+            freq[capacity[i]]++;
+         }
+
+         for(int i=50;i>0;i--){
+            while(freq[i]>0){
+               sum+=i;
+               count++;
+               if(sum>=appleCount){
                 return count;
+               }
+               freq[i]--;
             }
+         
         }
         return capacity.length;
     }
