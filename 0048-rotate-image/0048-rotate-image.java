b601ed048caa1aa1800c658056e12c041cae7de2
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2025-12-26 19:48:55 +0530

    Time: 0 ms (100%), Space: 42.1 MB (98.08%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,28 +1,23 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        boolean flag = true;
-        int n=matrix.length;
-
-       // int[][]  ans = new int[n][n];
-        
-        for(int i=0;i<n;i++){
-
-                for(int j=i;j<n;j++){
-
-                    int temp=matrix[i][j];
-                    matrix[i][j]=matrix[j][i];
-                    matrix[j][i]=temp;
-
-                }
-
-        }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n/2;j++){
-                int temp=matrix[i][j];
-                matrix[i][j]=matrix[i][n-1-j];
-                matrix[i][n-1-j]=temp;
+         int n = matrix.length;
+         int c = matrix[0].length;
+        // Transpose the matrix
+        for (int i = 0; i < n; i++) {
+              for (int j = i; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
             }
         }
-     
+        // Reverse each row
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - 1 - j];
+                matrix[i][n - 1 - j] = temp;
+            }
+        }
+
     }
 }
