00158cbbbb38bd5bc4ee75119d8d6111aa45e72b
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2025-12-27 22:53:39 +0530

    Time: 0 ms (100%), Space: 44 MB (34.55%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,34 +1,53 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-       
-        // int start=0;
-        // int end=(m*n)-1;
-        // while(start<=end){
-        //   int mid=(start+end)/2;
-        //   if(matrix[mid/n][mid%n]>target){
-        //     end = mid-1;
-        //   }else if(matrix[mid/n][mid%n]<target){
-        //     start=mid+1;
-        //   }else{
-        //     return true;
-        //   }
-        // }
-        // return false;
 
-       int n=matrix.length;
-      int m = matrix[0].length;
-      int i=0;
-      int j=m-1;
-      while(i<n && j>=0){
-        if(matrix[i][j]<target){
-           i++;  
-        }else if(matrix[i][j]>target){
-          j--;
-        }else{
-            return true;
+       //int n=matrix.length;
+      //int m = matrix[0].length;
+      //int i=0;
+      //int j=m-1;
+      //while(i<n && j>=0){
+        //if(matrix[i][j]<target){
+          // i++;  
+        //}else if(matrix[i][j]>target){
+          //j--;
+        //}else{
+          //  return true;
+        //}
+      //}
+        //return false;
+
+
+
+        int m=matrix.length;
+        int n=matrix[0].length;
+        int i=0;
+        int j=m-1;
+
+        while(i<=j){
+            int mid = (i+j)/2;
+            if(matrix[mid][0]<target && matrix[mid][n-1]>target){
+                break;
+            }else if(matrix[mid][0]>target){
+               j=mid-1;
+            }else{
+                i=mid+1;
+            }
         }
-      }
-        return false;
+        int row=(i+j)/2;
+        int l=0;
+        int r=n-1;
+         while(l<=r){
+            int mid=(l+r)/2;
+            if(matrix[row][mid]<target){
+                l=mid+1;
+            }else if(matrix[row][mid]>target){
+                r=mid-1;
+            }else{
+                return true;
+            }
+
+         }
+         return false;
 
         }
 
