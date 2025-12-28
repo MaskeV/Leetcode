0f865295286c7e60e63511b665ebcd2cd400381b
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2025-12-28 09:56:04 +0530

    Time: 1 ms (99.88%), Space: 46.7 MB (15.15%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -8,81 +8,61 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-// class Solution {
-//     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-//       ListNode join = new ListNode();
-//       ListNode start = join;
-//       int prev=0;
-//       while(l1!=null && l2!=null){
-       
-//        if(prev<10){
-//         start.next=new ListNode((l1.val+l2.val)%10);
-//         prev = l1.val+l2.val;
-//        }else{
-//         start.next=new ListNode((((l1.val+l2.val)%10)+(prev/10))%10);
-//         prev = l1.val+l2.val+(prev/10);
-//        }
-         
- 
-//         l1=l1.next;
-//         l2=l2.next;
-//         start = start.next;
-//       }
-//       while(l1!=null){ 
-//        if(prev<10){
-//         start.next=new ListNode((l1.val)%10);
-//         prev=l1.val;
-//        }else{
-//         start.next=new ListNode((((l1.val)%10)+(prev/10))%10);
-//         prev =l1.val+prev/10;
-//        }
-//        start = start.next;
-//        l1=l1.next;
-  
-//       }
-//          while(l2!=null){ 
-//        if(prev<10){
-//         start.next=new ListNode((l2.val)%10);
-//         prev=l2.val;
-//        }else{
-//         start.next=new ListNode((((l2.val)%10)+(prev/10))%10);
-//         prev =l2.val+prev/10;
-//        }
-//        start=start.next;
-//        l2=l2.next;
-  
-//       }
-      
-//       if(prev>=10){
-//         start.next = new ListNode(prev/10);
-//       }
-   
-    
-//        return join.next; 
-//     }
-// }
-
-
 class Solution{
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+       
         ListNode join = new ListNode();
-        ListNode mover = join;
-        int total=0,carry=0;
-
-        while(l1!=null || l2!=null || carry!=0){
-        total=carry;
-            if(l1!=null){
-                total += l1.val;
-                l1=l1.next;
-            }if(l2!=null){
-                total += l2.val;
-                l2=l2.next;
-            }
-            int num=total%10;
-            carry = total/10;
-            mover.next =  new ListNode(num);
-            mover = mover.next;
+        ListNode head = join;
+        int carry=0;
+        while(l1!=null && l2!=null){
+           int sum=l1.val+l2.val+carry;
+           join.next=new ListNode(sum%10); 
+           carry= sum/10;
+           join=join.next;
+           l1=l1.next;
+           l2=l2.next;
         }
-        return join.next;
+        while(l1!=null){
+           int sum=l1.val+carry;
+           join.next=new ListNode(sum%10); 
+           carry= sum/10;
+           join=join.next;
+           l1=l1.next;
+        }
+         while(l2!=null){
+            int sum=l2.val+carry;
+           join.next=new ListNode(sum%10); 
+           carry= sum/10;
+           join=join.next;
+           l2=l2.next;
+        }
+        if(carry!=0){
+           join.next=new ListNode(carry);  
+        }
+        return head.next;
     }
 }
+
+// class Solution{
+//     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+//         ListNode join = new ListNode();
+//         ListNode mover = join;
+//         int total=0,carry=0;
+
+//         while(l1!=null || l2!=null || carry!=0){
+//         total=carry;
+//             if(l1!=null){
+//                 total += l1.val;
+//                 l1=l1.next;
+//             }if(l2!=null){
+//                 total += l2.val;
+//                 l2=l2.next;
+//             }
+//             int num=total%10;
+//             carry = total/10;
+//             mover.next =  new ListNode(num);
+//             mover = mover.next;
+//         }
+//         return join.next;
+//     }
+// }
