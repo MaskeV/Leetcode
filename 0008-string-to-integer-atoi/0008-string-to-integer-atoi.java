6ab69587a275b135fd2c7392d33faf9bfe07cf53
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2025-12-31 16:04:58 +0530

    Time: 1 ms (99.82%), Space: 44 MB (14.76%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,67 +1,34 @@
 class Solution {
+   
     public int myAtoi(String s) {
-
-     int INT_MAX=Integer.MAX_VALUE;
-     int INT_MIN=Integer.MIN_VALUE;
-
-      int i = 0;
-      int n = s.length();
-
-      while(i<n && s.charAt(i)==' '){
-        i++;
-      }
-
-      if(i==n){
-        return 0;
-      }
-
-      int sign=1;
-      if(s.charAt(i)=='+'){
-        i++;
-      }else if(s.charAt(i)=='-'){
-        sign = -1;
-        i++;
-      }
-      long res =0;
-      while(i<n && Character.isDigit(s.charAt(i))){
-        int digit = s.charAt(i)-'0';
-        res = res*10+digit;
-        if(sign==-1 && sign*res<INT_MIN){
-            return INT_MIN;
-        }
-        if(sign==1 && sign*res>INT_MAX){
-            return INT_MAX;
-        }
-        i++;
-      }
-      return (int)(sign*res);
+        final int high = Integer.MAX_VALUE;
+        final int low = Integer.MIN_VALUE;
+         int flag =1; long ans=0;
+         boolean valid=true;
+         for(int i=0;i<s.length();i++){
+            if(valid && s.charAt(i)==' '){
+                continue;
+            }if(valid && s.charAt(i)=='+'){
+                valid=false;
+                continue;
+            }
+            if(valid && s.charAt(i)=='-' ){
+                 valid=false;
+                flag=-1;
+            }else if(s.charAt(i)-'0'>=0 && s.charAt(i)-'0'<10){
+                valid=false;
+                ans=(ans*10)+s.charAt(i)-'0';
+                if(ans * flag>= high){
+            return high;
+         }else if(ans * flag<=low){
+            return low;
+         }
+            }else{
+                break;
+            }
+           
+         }
+    
+        return (int) (ans*flag);
     }
 }
-
-
-// class Solution {
-//     public int myAtoi(String s) {
-//         int ans=0;
-//         boolean negative=false;
-//         boolean leadingZero=true;
-//         for(int i=0;i<s.length();i++){
-//             if(s.charAt(i)==' '){
-//                 continue;
-//             }if(s.charAt(i)=='-'){
-//                 negative=true;
-//             }else if(s.charAt(i)=='0' && leadingZero){
-//                 continue;
-//             }else if(s.charAt(i)-'0'>9){
-//                 break;
-//             }
-//             else if(s.charAt(i)!='0' || !leadingZero){
-//                 ans = ans*10 + (s.charAt(i)-'0');
-//                 leadingZero=false;
-//             }
-//         }
-//         if(negative){
-//             return ans*-1;
-//         }
-//         return ans;
-//     }
-// }
