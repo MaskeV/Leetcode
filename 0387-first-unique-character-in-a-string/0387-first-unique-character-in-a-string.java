a6262825a7a2021b8080e8903009d3f80b603d61
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2025-12-31 13:24:38 +0530

    Time: 6 ms (84.54%), Space: 47.1 MB (34.44%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -14,16 +14,27 @@ class Solution {
         //return -1;
 
 
-    Map<Character,Integer> map=new HashMap<>();
+   // Map<Character,Integer> map=new HashMap<>();
+    //for(char c:s.toCharArray()){
+    // map.put(c,map.getOrDefault(c,0)+1);
+    //}
+     //for(int i=0;i<s.length();i++){
+       // if(map.get(s.charAt(i))==1){
+         //   return i;
+        //}
+     //}
+    //return -1;
+
+    int [] arr=new int[26];
     for(char c:s.toCharArray()){
-     map.put(c,map.getOrDefault(c,0)+1);
+        arr[c-'a']++;
     }
+
      for(int i=0;i<s.length();i++){
-        if(map.get(s.charAt(i))==1){
+        if(arr[s.charAt(i)-'a']==1){
             return i;
         }
-     }
+    }
     return -1;
-
     }
 }
