e97312731ea8fc46c596c21eb809e73d2897b6fa
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2026-01-03 14:53:02 +0530

    Time: 2 ms (96.15%), Space: 42.4 MB (14.96%) - LeetHub

--- 1015-smallest-integer-divisible-by-k/1015-smallest-integer-divisible-by-k.java
+++ 1015-smallest-integer-divisible-by-k/1015-smallest-integer-divisible-by-k.java
@@ -6,7 +6,7 @@ class Solution {
         }
 
         int rem=0;
-        for(int i=0;i<=k;i++){
+        for(int i=0;i<k;i++){
             rem = (rem*10+1)%k;
             if(rem==0){
                 return i+1;
