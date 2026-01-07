3c10f76f3beb8eee5a42ec09c590ca2359bd3184
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2026-01-07 08:22:52 +0530

    Time: 3 ms (99.44%), Space: 46.7 MB (18.73%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,19 +1,38 @@
 class Solution {
     public String intToRoman(int num) {
-        final int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-        final String[] symbols = {"M", "CM", "D",  "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-
+        
+        int[] arr= {1,4,5,9,10,40,50,90,100,400,500,900,1000};
+        String[] roman = {"I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"};
         StringBuilder sb = new StringBuilder();
+        
 
-        for (int i = 0; i < values.length; ++i) {
-            if (num == 0)
-                break;
-            while (num >= values[i]) {
-                sb.append(symbols[i]);
-                num -= values[i];
+        //while (num>0){
+           // int i=0;
+           // while(i<arr.length-1){
+             //   if(arr[i]==num){
+                    //num=num-arr[i];
+                    //sb.append(roman[i]);
+                   // break;
+                 //}if(arr[i]>num){
+                    //num-=arr[i-1];
+                  //  sb.append(roman[i-1]);
+                //}
+              //  i++;
+            //}
+            //if(i==arr.length-1){
+              //  sb.append(roman[i]);
+            //    num-=arr[i];
+          //  }
+        //}
+
+
+        for(int i=arr.length-1;i>=0;i--){
+            while(num>=arr[i]){
+                sb.append(roman[i]);
+                num-=arr[i];
             }
         }
-
-        return sb.toString();        
+           return sb.toString();       
     }
+
 }
