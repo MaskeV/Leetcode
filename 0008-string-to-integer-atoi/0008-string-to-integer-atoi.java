9f86b1f03dd928e14a8d81eb2db129054e134100
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2026-01-08 17:11:48 +0530

    Time: 1 ms (99.77%), Space: 44 MB (27.86%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,34 +1,38 @@
 class Solution {
    
     public int myAtoi(String s) {
-        final int high = Integer.MAX_VALUE;
-        final int low = Integer.MIN_VALUE;
-         int flag =1; long ans=0;
-         boolean valid=true;
-         for(int i=0;i<s.length();i++){
-            if(valid && s.charAt(i)==' '){
-                continue;
-            }if(valid && s.charAt(i)=='+'){
-                valid=false;
-                continue;
-            }
-            if(valid && s.charAt(i)=='-' ){
-                 valid=false;
-                flag=-1;
-            }else if(s.charAt(i)-'0'>=0 && s.charAt(i)-'0'<10){
-                valid=false;
-                ans=(ans*10)+s.charAt(i)-'0';
-                if(ans * flag>= high){
-            return high;
-         }else if(ans * flag<=low){
-            return low;
-         }
-            }else{
-                break;
-            }
+     Boolean flag=true;
+     int negative =1;
+     long no=0;
+
+      for(int i=0;i<s.length();i++){
+        int temp=s.charAt(i);
+        if(s.charAt(i)==' '  && flag){
+            continue;
+        }
+        if(s.charAt(i)=='+' && flag){
+            flag=false;
+            continue;
+        }
+        if(s.charAt(i)=='-' && flag){
+          negative=-1;
+          flag=false;
+        }
+        else if(s.charAt(i)>='0' && s.charAt(i)<='9'){
            
-         }
-    
-        return (int) (ans*flag);
+            if((no*10+s.charAt(i)-'0') *negative >= Integer.MAX_VALUE){
+                return Integer.MAX_VALUE; 
+            }if((no*10+s.charAt(i)-'0')*negative <=Integer.MIN_VALUE){
+                return Integer.MIN_VALUE;
+            }
+            no=no*10+(s.charAt(i)-'0');
+            flag=false;
+        }else{
+            break;
+        }
+
+      }
+      return (int) no*negative;
+
     }
 }
