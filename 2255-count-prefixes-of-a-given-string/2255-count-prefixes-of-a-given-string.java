05bf77efe87e055b63fd3fd31ff50c97272cf81f
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2026-01-13 15:04:26 +0530

    Time: 1 ms (61.63%), Space: 45.2 MB (7.43%) - LeetHub

--- 2255-count-prefixes-of-a-given-string/2255-count-prefixes-of-a-given-string.java
+++ 2255-count-prefixes-of-a-given-string/2255-count-prefixes-of-a-given-string.java
@@ -5,11 +5,26 @@ class Solution {
         for(int i=0;i<words.length;i++){
 
             String str = words[i];
-            if(s.startsWith(str)){
+           // if(s.startsWith(str)){
+            if(isPrefix(s,str)){
                 count++;
             }
 
         }
         return count;
+    }
+
+    boolean isPrefix(String s,String str){
+        if(s.length()<str.length()){
+            return false;
+        }
+       int i=0;
+        while(i<str.length()){
+            if(s.charAt(i)!=str.charAt(i)){
+                return false;
+            }
+            i++;
+        }
+        return true;
     }
 }
