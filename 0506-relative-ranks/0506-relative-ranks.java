aa5132a90f84a69fcbe63542ca67cdf198a964ee
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2026-01-20 16:18:00 +0530

    Time: 93 ms (6.82%), Space: 48 MB (12.6%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.java
+++ 0506-relative-ranks/0506-relative-ranks.java
@@ -2,7 +2,9 @@ class Solution {
     public String[] findRelativeRanks(int[] score) {
         String[] ans = new String[score.length];
 
-        Integer [] sorted = new Integer[score.length];
+        //Integer [] sorted = new Integer[score.length];  use Integer instead of int  if you need to use collections 
+
+        int[] sorted = new int[score.length];
         int id =0;
 
        
@@ -11,20 +13,20 @@ class Solution {
             id++;
         }
 
-         Arrays.sort(sorted,Collections.reverseOrder());
-
+          //Arrays.sort(sorted,Collections.reverseOrder());
+          Arrays.sort(sorted);
 
         for(int i=0;i<score.length;i++){
-            for(int j=0;j<score.length;j++){
+            for(int j=sorted.length-1;j>=0;j--){
                 if(score[i]==sorted[j]){
-                    if(j==0){
+                    if(j==sorted.length-1){
                         ans[i]="Gold Medal";
-                    }else if(j==1){
+                    }else if(j==sorted.length-2){
                         ans[i]="Silver Medal";
-                    }else if(j==2){
+                    }else if(j==sorted.length-3){
                         ans[i]="Bronze Medal";
                     }else{
-                        ans[i]=Integer.toString(j+1);
+                        ans[i]=Integer.toString(sorted.length-j);
                     }
 
                 }
