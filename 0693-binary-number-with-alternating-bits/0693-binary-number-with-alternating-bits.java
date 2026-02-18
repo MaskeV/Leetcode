82f6284872c3938004ac87f9f218fbb67d3303a2
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2026-02-18 10:14:29 +0530

    Time: 1 ms (17.77%), Space: 41.9 MB (97.08%) - LeetHub

---
+++ 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean hasAlternatingBits(int n) {
+        StringBuilder sb = new StringBuilder();
+
+        while(n>0){
+            sb.append(n%2);
+            n=n/2;
+        } 
+
+        for(int i=0;i<sb.length()-1;i++){
+            if(sb.charAt(i)==sb.charAt(i+1)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
