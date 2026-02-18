7c088f0ebf4de17e1b6d44f4386bbb660ff0d54b
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2026-02-18 20:33:47 +0530

    Time: 1 ms (99.32%), Space: 44.2 MB (50%) - LeetHub

--- 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.java
+++ 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.java
@@ -1,18 +1,38 @@
 class Solution {
     public int maxFrequencyElements(int[] nums) {
-        Map<Integer,Integer> map =new HashMap<>();
+        int[] freq = new int[101];
         int max=0;
         for(int i:nums){
-            map.put(i,map.getOrDefault(i,0)+1);
-            max=map.get(i)>max ? map.get(i):max;
+            freq[i]++;
+            max=freq[i]>max ? freq[i]:max;
         }
-      
         int sum=0;
-        for(int i:map.keySet()){
-            if(map.get(i)==max){
-                sum+=max;
+        for(int i=0;i<101;i++){
+            if(freq[i]==max){
+             sum+=freq[i];
             }
         }
-        return sum;
+return sum;
     }
 }
+
+
+
+// class Solution {
+//     public int maxFrequencyElements(int[] nums) {
+//         Map<Integer,Integer> map =new HashMap<>();
+//         int max=0;
+//         for(int i:nums){
+//             map.put(i,map.getOrDefault(i,0)+1);
+//             max=map.get(i)>max ? map.get(i):max;
+//         }
+      
+//         int sum=0;
+//         for(int i:map.keySet()){
+//             if(map.get(i)==max){
+//                 sum+=max;
+//             }
+//         }
+//         return sum;
+//     }
+// }
