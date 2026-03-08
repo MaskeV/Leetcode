fec6443611a6037052ff967176a4b5ba2afb69ee
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2026-03-08 23:20:43 +0530

    Time: 1 ms (99.93%), Space: 47 MB (63.57%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,10 +1,37 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int ans=0;
-        for(int i:nums){
-         ans = ans ^ i;
-        }
-        return ans;
-
+       int ans=0;
+       for(int i=0;i<nums.length;i++){
+        ans = ans ^ nums[i];
+       }
+       return ans;
     } 
 }
+// class Solution {
+//     public int singleNumber(int[] nums) {
+//         Map<Integer,Integer> map = new HashMap<>();
+//         for(int i:nums){
+//          map.put(i,map.getOrDefault(i,0)+1);
+//         }
+//   for(int i:map.keySet()){
+//     if(map.get(i)==1){
+//         return i;
+//     }
+//   }
+//   return -1;
+//     } 
+// }
+
+// for (int i = 0; i < nums.length; i++) {
+//     boolean unique = true;
+//     for (int j = 0; j < nums.length; j++) {
+//         if (i != j && nums[i] == nums[j]) { // another match found
+//             unique = false;
+//             break;
+//         }
+//     }
+//     if (unique) {
+//         return nums[i];
+//     }
+// }
+// return 0; 
