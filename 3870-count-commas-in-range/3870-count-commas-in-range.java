bdd07865febc5fb295cd482bc70cff40252863cc
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2026-03-15 11:12:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,16 +1,9 @@
 class Solution {
     public int countCommas(int n) {
-        int orignal =n;
-        int digit=0;
-        while(n>0){
-            digit++;
-            n = n/10;
-        }
-        if(digit<4){
-          return 0;
-        }
-
-   return orignal -1000+1;
+      if(n<1000){
+        return 0;
+      }
+       return n-1000+1;
 
     }
 }
