f064c321587c36b81f08fb94b241824f1c0a9576
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2026-03-20 18:45:12 +0530

    Time: 4 ms (99.89%), Space: 46.1 MB (28.66%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -3,8 +3,8 @@ class Solution {
         int actual = x;
         int rev=0;
         while(x>0){
-rev = rev*10+(x%10);
-x=x/10;
+           rev = rev*10+(x%10);
+           x=x/10;
         }
         if(actual==rev){
             return true;
