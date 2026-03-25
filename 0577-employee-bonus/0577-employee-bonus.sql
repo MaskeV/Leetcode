ba4f8be4e7da6b719cc3a8e70468fdffade0c7b5
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2026-03-25 21:12:58 +0530

    Time: 883 ms (80.7%), Space: 0B (100%) - LeetHub

--- 0577-employee-bonus/0577-employee-bonus.sql
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -1,4 +1,4 @@
-select name, bonus
+select name ,  bonus
 from Employee E left join Bonus B
-on E.empId=B.empId
-where bonus<1000 or bonus is null
+on E.empId = B.empId
+where bonus is null or bonus <1000
