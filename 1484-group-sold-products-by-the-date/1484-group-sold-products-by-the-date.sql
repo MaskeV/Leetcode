4aea7078d991355bcdcc6175ccb7fe02877d914a
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2026-03-26 00:52:42 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1484-group-sold-products-by-the-date/1484-group-sold-products-by-the-date.sql
+++ 1484-group-sold-products-by-the-date/1484-group-sold-products-by-the-date.sql
@@ -1,4 +1,4 @@
-select sell_date, count(distinct product) as num_sold, Group_concat(distinct product)  as products
+select sell_date, count(distinct product) as num_sold, Group_concat(distinct product order by product )  as products
 from Activities
 group by sell_date
 
