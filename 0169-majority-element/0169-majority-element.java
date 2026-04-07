eba31d0033e5e9679f137b984401c42939eafeee
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2026-04-07 22:49:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,7 +1,7 @@
 class Solution {
     public int majorityElement(int[] nums) {
         int count = 0;
-        int target = nums[0];
+        int target = 0;
 
         for(int i=0;i<nums.length;i++){
             if(count<=0){
