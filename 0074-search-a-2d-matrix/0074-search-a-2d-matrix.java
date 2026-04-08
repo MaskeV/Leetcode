194e9c32ffd4aad77a3078265c1341d623a2a24a
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2026-04-09 01:03:40 +0530

    Time: 0 ms (100%), Space: 44 MB (50.65%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,54 +1,33 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+           
+      int i=0;
+      
+      while(i<m && matrix[i][n-1]<target){
+              i++;
+       }
 
-       //int n=matrix.length;
-      //int m = matrix[0].length;
-      //int i=0;
-      //int j=m-1;
-      //while(i<n && j>=0){
-        //if(matrix[i][j]<target){
-          // i++;  
-        //}else if(matrix[i][j]>target){
-          //j--;
-        //}else{
-          //  return true;
-        //}
-      //}
-        //return false;
+       if(i==m){
+         return false;
+       }
 
+        int low =0;int high = n-1;
 
-
-        int m=matrix.length;
-        int n=matrix[0].length;
-        int i=0;
-        int j=m-1;
-
-        while(i<=j){
-            int mid = (i+j)/2;
-            if(matrix[mid][0]<target && matrix[mid][n-1]>target){
-                break;
-            }else if(matrix[mid][0]>target){
-               j=mid-1;
-            }else{
-                i=mid+1;
-            }
-        }
-        int row=(i+j)/2;
-        int l=0;
-        int r=n-1;
-         while(l<=r){
-            int mid=(l+r)/2;
-            if(matrix[row][mid]<target){
-                l=mid+1;
-            }else if(matrix[row][mid]>target){
-                r=mid-1;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(matrix[i][mid]>target){
+                high = mid-1;
+            }else if(matrix[i][mid]<target){
+               low = mid+1;
             }else{
                 return true;
             }
-
-         }
-         return false;
-
         }
 
+
+      return false;
+        
+    }
 }
