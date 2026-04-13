9ea554c93e7a2fff76324475df3e238e701bf39c
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2026-04-13 23:46:08 +0530

    Time: 2 ms (9.13%), Space: 42.9 MB (73.66%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,6 +1,12 @@
 class Solution {
+
+
     public int lengthOfLastWord(String s) {
-        String[]  arr = s.split(" ");
+
+        String[] arr = s.trim().split("\\s+");
         return arr[arr.length-1].length();
+
+
+        
     }
 }
