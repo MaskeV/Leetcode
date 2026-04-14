eb41e0eec1241e34174e3360cbf291f45e5ae491
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2026-04-14 20:28:16 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -1,25 +1,38 @@
 class Solution {
     public int hIndex(int[] citations) {
 
-        int[] freq = new int[1001];
-        int max=0;
+        //int[] freq = new int[1001];
+        //int max=0;
 
-        for(int i:citations){
-            for(int j=0;j<=i;j++){
-            freq[j]++;
-            }
-            if(max<i){
-                max=i;
-            }
-        }
+        //for(int i:citations){
+          //  for(int j=0;j<=i;j++){
+          //  freq[j]++;
+            //}
+            //if(max<i){
+            //    max=i;
+            //}
+        //}
         
 
-        for(int i=max;i>=0;i--){
-            if(freq[i]>=i){
-                return i;
+       // for(int i=max;i>=0;i--){
+         //   if(freq[i]>=i){
+           //     return i;
+            //}
+        //}
+
+        //return 1;
+
+        int n = citations.length;
+
+        Arrays.sort(citations);
+        for(int i=0;i<n;i++){
+            int h=n-i;
+
+            if(citations[i]>=h){
+                return  h;
             }
         }
 
-        return 1;
+        return 0;
     }
 }
