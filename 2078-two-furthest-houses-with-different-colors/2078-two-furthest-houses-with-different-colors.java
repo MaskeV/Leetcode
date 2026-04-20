7d6a5e134bca90a666f2f822342fa20836b493c1
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2026-04-20 10:52:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.java
+++ 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.java
@@ -1,17 +1,35 @@
 class Solution {
     public int maxDistance(int[] colors) {
-        int n = colors.length;
-        int max = 0;
-    for(int i=0;i<n;i++){
-        for(int j=n-1;j>=i;j--){
-            if(colors[i]!=colors[j]){
-                int dis = j-i;
-                max = Math.max(max,dis);
-                break;
-            }
-        }
-    }
+      //  int n = colors.length;
+        //int max = 0;
+    //for(int i=0;i<n;i++){
+      //  for(int j=n-1;j>=i;j--){
+        //    if(colors[i]!=colors[j]){
+          //      int dis = j-i;
+            //    max = Math.max(max,dis);
+              //  break;
+            //}
+   //     }
+ //   }
         
+
+       int left=0;
+       int right=colors.length-1;
+       int max=0;
+
+        while(colors[left]==colors[right]){
+            right--;
+        }
+
+        max  =  right-left;
+
+        right = colors.length-1;
+
+        while(colors[left]==colors[right]){
+            left++;
+        }
+
+        max = Math.max(right-left,max);
         return max;
     }
 }
