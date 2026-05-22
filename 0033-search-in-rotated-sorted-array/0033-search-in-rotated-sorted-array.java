9cf0656277a8d4626217da2e7c5dfdfd8a95038c
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2026-05-22 23:58:27 +0530

    Time: 0 ms (100%), Space: 43.8 MB (63.65%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,75 +1,29 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int start=0,end=nums.length-1;
-        int pivot = maxElement(nums);
-        if(nums[pivot]==target){
-            return pivot;
-        }else if(nums[start]<=target){
-            return binarySearch(nums,target,0,pivot-1);
-        }else{
-            return binarySearch(nums,target,pivot+1,nums.length-1);
-        }
-   
+        int n = nums.length;
+        int lo = 0, hi = n - 1;
 
-    }
+        while (lo < hi) {
+            int mid = (lo + hi) / 2;
+            
+            if (nums[mid] > nums[n - 1]) lo = mid + 1;
+            else hi = mid;
+        }
 
-    private int maxElement(int[] nums){
-          int start=0,end=nums.length-1;
-        int max=0;
-        while(start<=end){
-            int mid=start+(end-start)/2;
-            if( mid<end && nums[mid]>nums[mid+1]){
-            return mid;
-            }else if(mid>start && nums[mid]<nums[mid-1]){
-                return mid-1;
-            }else if(nums[start]>=nums[mid]){
-                end=mid-1;
-            }else{
-                start=mid+1;
-            }
+        int rot = lo;
+        lo = 0; hi = n - 1;
+
+        while (lo <= hi) {
+            int mid = (lo + hi) / 2;
+            int real = (mid + rot) % n;
+
+            if (nums[real] == target)
+                return real;
+            
+            if (nums[real] < target) lo = mid + 1;
+            else hi = mid - 1;
         }
-        return nums.length-1;
-    }
-    private int binarySearch(int[] nums,int target,int start,int end){
-       while(start<=end){
-        int mid=start+(end-start)/2;
-        if(nums[mid]>target){
-            end=mid-1;
-        }else if(nums[mid]<target){
-            start=mid+1;
-        }else{
-            return mid;
-        }
-       
-       }
+
         return -1;
     }
 }
-
-
-// class Solution {
-//     public int search(int[] nums, int target) {
-//        int  start=0,end=nums.length-1;
-//         while(start<=end){
-//             int mid = start+(end-start)/2;
-//             if(nums[mid]==target){
-//                 return mid;
-//             }else if(nums[start]<=nums[mid]){
-//                 if(nums[start]<=target && nums[mid]>=target){
-//                   end =mid-1;
-//                 }else{
-//                     start=mid+1;
-//                 }
-//             }else{
-//                 if(nums[end]>=target && target>=nums[mid]){
-//                     start=mid+1;
-//                 }else{
-//                     end=mid-1;
-//                 }
-//             }
-
-//             }
-        
-//             return -1;
-//      }
-// }
