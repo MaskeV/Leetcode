e8da54c59e3ba91c66f11eb5710bb9690ce30ff1
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2026-05-29 13:10:05 +0530

    Time: 3 ms (21.11%), Space: 47.6 MB (61.35%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,62 +1,28 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-     int  rows = matrix.length;
-     int cols = matrix[0].length;
-     boolean zeroinFirstCol=false;
 
-     for(int i=0;i<rows;i++){
-        if(matrix[i][0]==0){
-            zeroinFirstCol=true;
-        }
-        for(int j=01;j<cols;j++){
-            if(matrix[i][j]==0){
-                matrix[0][j]=0;
-                matrix[i][0]=0;
+        Set<Integer> row = new HashSet<>();
+          Set<Integer> column = new HashSet<>();
+
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+               if(matrix[i][j]==0){
+                row.add(i);
+                column.add(j);
+               }
             }
         }
-        }
-        for(int i=rows-1;i>=0;i--){
-            for(int j=cols-1;j>=1;j--){
-                if(matrix[i][0]==0 || matrix[0][j]==0){
-                    matrix[i][j]=0;
-                }
-            }
-            if(zeroinFirstCol){
-                matrix[i][0] =  0;
+        for(int i:row){
+            for(int j=0;j<matrix[0].length;j++){
+                matrix[i][j]=0;
             }
         }
-    
+
+        for(int j:column){
+            for(int i=0;i<matrix.length;i++){
+                matrix[i][j]=0;
+            }
+        }
+
     }
 }
-
-
-
-
-// class Solution {
-//     public void setZeroes(int[][] matrix) {
-//      int  rows = matrix.length;
-//      int cols = matrix[0].length;
-
-//      Set<Integer> zrows = new HashSet<>();
-//      Set<Integer> zcols = new HashSet<>();
-
-//      for(int i=0;i<rows;i++){
-//         for(int j=0;j<cols;j++){
-//             if(matrix[i][j]==0){
-//                 zrows.add(i);
-//                 zcols.add(j);
-//             }
-//         }
-//      }
-
-//      for(int i=0;i<rows;i++){
-//         for(int j=0;j<cols;j++){
-//             if(zrows.contains(i) || zcols.contains(j)){
-//                 matrix[i][j]=0;
-//             }
-//         }
-//      }
-
-
-//     }
-// }
