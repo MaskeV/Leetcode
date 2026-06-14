7e9a419f0711d931e58c516646e48d69b1c93f8c
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2026-06-14 23:21:17 +0530

    Time: 12 ms (10.32%), Space: 115.7 MB (16.02%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.java
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.java
@@ -8,53 +8,27 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-// class Solution {
-//     public int pairSum(ListNode head) {
-//         ListNode mover = head;
-//         List<Integer> arr = new ArrayList<>();
-//         while(mover!=null){
-//             arr.add(mover.val);
-//             mover = mover.next;
-//         }
-//         int max=0;
-//         int n = arr.size();
-
-//         for(int i=0;i<n/2;i++){
-//             int sum = arr.get(i) + arr.get(n-1-i);
-//             max = Math.max(sum,max);   
-//         }
-        
-//         return max;
-//     }
-// }
-
 class Solution {
     public int pairSum(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head;
-        while(fast!=null && fast.next!=null){
-            slow=slow.next;
-            fast=fast.next.next;
-        }
-        ListNode mid = slow;
-        ListNode prev = null;
-        ListNode pres=mid;
-        ListNode next = mid.next;
-        while(pres!=null){
-            pres.next =prev;
-            prev=pres;
-            pres = next;
-            if(next!=null){
-                next = next.next;
+
+        int maxtwinSum = 0;
+        List<Integer> arr = new ArrayList<>();
+
+        ListNode temp = head;
+         while(temp!=null){
+            arr.add(temp.val);
+            temp =  temp.next;  
+         }
+         
+         int n  = arr.size();
+         
+
+         for(int i=0;i<n/2;i++){
+            if(arr.get(i)+arr.get(n-i-1)>maxtwinSum){
+                maxtwinSum = arr.get(i)+arr.get(n-i-1);
             }
-        }
-        int max=0;
-        while(prev!=null){
-            int sum = head.val + prev.val;
-            max = Math.max(sum,max);
-            head = head.next;
-            prev = prev.next;
-        }
-        return max;
+         }
+
+        return maxtwinSum;
     }
 }
