d48e67942087951499f7dabe64941eb6c6889fed
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2026-06-15 10:58:20 +0530

    Time: 0 ms (100%), Space: 44.1 MB (66.69%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -8,72 +8,24 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-// class Solution {
-//     public ListNode reverseList(ListNode head) {
-       
-        // ArrayList<Integer> arr = new ArrayList<>();
-        // while(head!=null){
-        //     arr.add(head.val);
-        //     head=head.next;
-        // }
-    
-        // Collections.reverse(arr);
-        // ListNode rev = new ListNode();
-        // ListNode start = rev;
-        
-        // for(int i : arr){
-        //     rev.next = new ListNode(i);
-        //     rev = rev.next;
-        // }
-                                                            
-        // return start.next;
-
-        
-//         ListNode rev = null;
-//         ListNode mover = head;
-//         while(mover!=null){
-//             ListNode temp = new ListNode(mover.val);
-//             temp.next = rev;
-//             rev=temp;
-//             mover=mover.next;
-//              }
-//              return rev;
-//     }
-// }
-
-
-
-
-class Solution{
+class Solution {
     public ListNode reverseList(ListNode head) {
+
         if(head==null){
             return null;
         }
-        ListNode prev = null;
-        ListNode curr = head;
-        ListNode next = head.next;
+        ListNode curr = head, prev = null, next = head.next;
 
         while(curr!=null){
-            curr.next=prev;
-            prev=curr;
-            curr=next;
-            if(next!=null){
-                next = next.next;
-            }
+           curr.next = prev;
+           prev = curr;
+           curr = next;
+           if(next!=null){
+            next = next.next;
+           }
         }
+
         return prev;
-    }
         
     }
-    // ListNode mover = head;
-    // ListNode rev = null;
-    // while(mover!=null){
-    //     ListNode temp = new ListNode(mover.val);
-    //     temp.next=rev;
-    //     rev = temp;
-    //     mover = mover.next;
-    // }
-    // return rev;
-    // }
-    
-// }
+}
