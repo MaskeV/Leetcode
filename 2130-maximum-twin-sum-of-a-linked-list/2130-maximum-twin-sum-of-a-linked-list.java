24bfff031674bf14f8907a71a1214a4e1184e597
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2026-06-15 11:33:24 +0530

    Time: 4 ms (98.31%), Space: 101 MB (81.1%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.java
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.java
@@ -11,24 +11,66 @@
 class Solution {
     public int pairSum(ListNode head) {
 
-        int maxtwinSum = 0;
-        List<Integer> arr = new ArrayList<>();
+        //int maxtwinSum = 0;
+        //List<Integer> arr = new ArrayList<>();
 
-        ListNode temp = head;
-         while(temp!=null){
-            arr.add(temp.val);
-            temp =  temp.next;  
-         }
+        //ListNode temp = head;
+         //while(temp!=null){
+           // arr.add(temp.val);
+            //temp =  temp.next;  
+         //}
          
-         int n  = arr.size();
+        // int n  = arr.size();
          
 
-         for(int i=0;i<n/2;i++){
-            if(arr.get(i)+arr.get(n-i-1)>maxtwinSum){
-                maxtwinSum = arr.get(i)+arr.get(n-i-1);
-            }
-         }
+         //for(int i=0;i<n/2;i++){
+         //   if(arr.get(i)+arr.get(n-i-1)>maxtwinSum){
+           //     maxtwinSum = arr.get(i)+arr.get(n-i-1);
+            //}
+         //}
 
-        return maxtwinSum;
+       // return maxtwinSum;
+
+
+       int maxTwinSum=0;
+
+
+       ListNode fast = head, slow = head;
+
+       while(fast!=null){
+        fast = fast.next.next;
+        slow = slow.next;
+       }
+
+       ListNode prev = null,next=null;
+
+       if(slow!=null){
+        next = slow.next;
+       }
+
+       while(slow!=null){
+         slow.next=prev;
+          prev = slow;
+          slow = next;
+          if(next!=null){
+          next = next.next;
+          }
+
+       }
+
+      ListNode temp = prev;
+       while(head!=temp && prev!=null){
+        if(head.val+prev.val>maxTwinSum){
+            maxTwinSum = head.val+prev.val;
+
+        }
+            head = head.next;
+            prev=prev.next;
+       }
+
+    
+
+
+       return maxTwinSum;
     }
 }
