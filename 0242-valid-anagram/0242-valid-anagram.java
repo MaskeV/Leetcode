04e06de5e8777ce5d4d166153af933cfa0dfe3b1
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2026-07-02 12:17:57 +0530

    Time: 4 ms (82.22%), Space: 44.1 MB (89.54%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,36 +1,22 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-     // Set<Character> set=new HashSet<>();
-      //if(s.length()!=t.length()){
-        //return false;
-     // }
-     // for(char c:s.toCharArray()){
-        //set.add(c);
-      //}
-      //for(int i=0;i<t.length();i++){
-        //if(!set.contains(t.charAt(i))){
-            //return false;
-       // }
-     // }
 
-       // return true; 
+        int[] freq = new int[26];
 
-
-     if(s.length()!=t.length()){
-        return false;
-     }
-      int[] arr = new int[26];
-      for(int i=0;i<s.length();i++){
-        arr[s.charAt(i)-'a']++;
-        arr[t.charAt(i)-'a']--;
-      }
-
-      for(int i=0;i<26;i++){
-        if(arr[i]!=0){
-            return false;
+        for(int i=0;i<s.length();i++){
+            freq[s.charAt(i)-'a']++;
         }
-      }
-      return true;
+
+        for(int i=0;i<t.length();i++){
+            freq[t.charAt(i)-'a']--;
+        }
+
+        for(int i=0;i<26;i++){
+            if(freq[i]!=0){
+                return false;
+            }
+        }
         
+        return true;
     }
 }
