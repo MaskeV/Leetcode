d35510b3a238e65fc36520153a548260185e6e59
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2026-07-17 23:21:29 +0530

    Time: 3 ms (55.51%), Space: 46.7 MB (36.86%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,42 +1,48 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        // Set<Integer> row = new HashSet<>();
-        // Set<Integer> column = new HashSet<>();
-        // Set<Integer> block = new HashSet<>();
-        Set set = new HashSet<>();
+       for(int i=0;i<9;i++){
 
-        for(int i=0;i<9;i++){
-            for(int j=0;j<9;j++){
-                char num=board[i][j];
-                if(num!='.'){
-                    if(!set.add(num+"in row"+i) || !set.add(num+"in column"+j)|| !set.add(num +"in block"+ i/3 + "-" + j/3)){
+          HashSet<Character> row = new HashSet<>();
+          HashSet<Character> col = new HashSet<>();
+
+          for(int j=0;j<9;j++){
+
+            if(board[i][j]!='.'){
+           if(row.contains(board[i][j])){
+            return false;
+           }
+            row.add(board[i][j]);
+            }
+
+            if(board[j][i]!='.'){
+
+           if(col.contains(board[j][i])){
+            return false;
+           }
+            col.add(board[j][i]);
+            }
+          }
+       }
+
+
+      for(int i=0;i<9;i+=3){
+        for(int j=0;j<9;j+=3){
+            Set<Character> block  = new HashSet<>();
+          for(int k=i;k<i+3;k++){
+            for(int l=j;l<j+3;l++){
+                if(board[k][l]!='.'){
+                    if(block.contains(board[k][l])){
                         return false;
                     }
+
+                    block.add(board[k][l]);
                 }
             }
+          }
         }
-        return true;
+      }
 
+
+       return true;
     }
 }
-
-
-
-
-
-// class Solution {
-//     public boolean isValidSudoku(char[][] board) {
-//          Set seen = new HashSet();
-//     for (int i=0; i<9; ++i) {
-//         for (int j=0; j<9; ++j) {
-//             char number = board[i][j];
-//             if (number != '.')
-//                 if (!seen.add(number + " in row " + i) ||
-//                     !seen.add(number + " in column " + j) ||
-//                     !seen.add(number + " in block " + i/3 + "-" + j/3))
-//                     return false;
-//         }
-//     }
-//     return true;
-//     }
-// }
