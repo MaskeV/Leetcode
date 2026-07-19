c48931b98b3e4ac0c1a9c65e14c1d9f914232986
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2026-07-20 00:36:54 +0530

    Time: 2 ms (99.3%), Space: 44 MB (92.14%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,15 +1,30 @@
 class Solution {
     public boolean isPalindrome(String s) {
-    s=s.toLowerCase().replaceAll("[^a-z0-9]","");
-     int i=0,j=s.length()-1;
-     while(i<j){
-        if(s.charAt(i)!=s.charAt(j)){
-            return false;
-        }
-        i++;
-        j--;
-     }
-     return true;
-    }
 
+      int i=0, j= s.length()-1;
+
+      while(i<j){
+
+        char start = s.charAt(i);
+        char end = s.charAt(j);
+
+        if(!Character.isLetterOrDigit(start)){
+            i++;
+        }else if(!Character.isLetterOrDigit(end)){
+            j--;
+        }else{
+
+            if(Character.toLowerCase(start) != Character.toLowerCase(end)){
+                return false;
+            }
+            
+            i++;
+            j--;
+
+        }
+      }  
+
+      return true;
+        
+    }
 }
