df4e9ffdceec6e7b685ea26ebf9d9f3b33dc9865
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2026-07-24 23:21:18 +0530

    Time: 383 ms (5.07%), Space: 47.8 MB (10.36%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,29 +1,30 @@
 class Solution {
     public String frequencySort(String s) {
 
-        Map<Character,Integer> map = new HashMap<>();
-    
-        for(int i=0;i<s.length();i++){
-           map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
+      Map<Character, Integer> map = new HashMap<>();
+      
+      for(int i=0;i<s.length();i++){
+        char c = s.charAt(i);
+        map.put(c,map.getOrDefault(c,0)+1);
+      }
+
+      List<Character> temp = new ArrayList<>();
+
+      for(char c:map.keySet()){
+        temp.add(c);
+      }
+
+      temp.sort((a,b) ->  map.get(b).compareTo(map.get(a)));
+
+      String ans ="";
+
+      for(char c:temp){
+        for(int i=0;i<map.get(c);i++){
+            ans += c;
         }
+      }
+ 
+      return ans;
 
-        List<Character> list = new ArrayList<>();
-
-        for(char c:map.keySet()){
-            list.add(c);
-        }
-
-        list.sort((a,b)-> map.get(b).compareTo(map.get(a)));
-
-        String ans = "";
-
-        for(char c:list){
-            int n = map.get(c);
-            for(int i=0;i<n;i++){
-                ans = ans+c;
-            }
-        }      
-
-        return ans; 
     }
 }
