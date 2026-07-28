09d5a9fc49f400b8ba29095b5e57b960048b7496
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2026-07-28 13:19:05 +0530

    Time: 430 ms (13.89%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,47 +1,8 @@
-select E.name
-from Employee E join Employee M
-on E.id = M.managerId 
-group by E.id
-having count(M.id)>=5
+# Write your MySQL query statement below
 
-
-
-
-
-
-
-
-
-
--- select  M.name
--- from Employee E left join Employee M
--- on E.managerId=M.id
--- group by M.id
--- having count(M.id)>=5
-
-
--- select name
--- from Employee
--- where id in (select managerId from Employee  group by managerId having count(managerId)>=5);
-
-
--- SELECT m.name
--- FROM Employee e  JOIN Employee m 
--- ON e.managerId=m.id
--- GROUP BY e.managerId
--- HAVING COUNT(e.managerId) >= 5
-
-
--- select M.name
--- from Employee E join Employee M
--- on E.managerId=M.id
--- group by M.id
--- having count(M.id)>=5
-
-
-
-
-
-
-
+select E2.name 
+from Employee E1 join Employee E2
+on E1.managerId=E2.id
+group by E1.managerId
+having count(E1.id)>=5;
 
