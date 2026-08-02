95b31fbe35c9b8d2a39dd70aded265e38004ef78
Vaishnavi Maske <153220050+MaskeV@users.noreply.github.com>
2026-08-02 19:42:46 +0530

    Time: 1240 ms (6.29%), Space: 0B (100%) - LeetHub

--- 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -1,20 +1,8 @@
 # Write your MySQL query statement below
--- select round(avg(if(order_date=customer_pref_delivery_date,1,0))*100,2) as immediate_percentage
--- from  Delivery
--- where (customer_id ,order_date)
--- in (select customer_id,min(order_date) as first_orderdate
--- from Delivery
--- group by customer_id );
--- # Select where () in () 
--- # 2*100/4
-
-
-select round(((count(customer_id)/(select count(distinct customer_id) from Delivery))*100),2) as immediate_percentage
+select round((count(customer_id)/(select count(distinct customer_id) from Delivery))*100,2) as immediate_percentage
+from Delivery 
+where (customer_id,customer_pref_delivery_date) in
+(select customer_id, min(order_date)
 from Delivery
-where customer_id in 
-(select customer_id
-from Delivery
-group by customer_id
-having customer_pref_delivery_date=min(order_date));
-
+group by customer_id)
 
